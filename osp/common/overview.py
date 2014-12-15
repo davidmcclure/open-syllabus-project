7f53e500aaa2e9ea54b84287c39bac8496fcce76
David McClure <davidwilliammcclure@gmail.com>
2014-12-15 11:21:20 -0800

    Stub for an Overview service adapter.

---
+++ osp/common/overview.py
@@ -0,0 +1,58 @@
+
+
+class Overview:
+
+
+    def put_store_state(self, state):
+
+        """
+        Set the store state.
+
+        :param state: The new state.
+        """
+
+        pass
+
+
+    def get_store_state(self):
+
+        """
+        Retrieve the store state.
+        """
+
+        pass
+
+
+    def put_store_object(self, i_long, i_string, json):
+
+        """
+        Set a store object
+
+        :param i_long: An indexed numeric field.
+        :param i_string: An indexed string field.
+        :param json: JSON for the object.
+        """
+
+        pass
+
+
+    def get_store_object(self, id):
+
+        """
+        Retrieve a store object
+
+        :param id: The object id.
+        """
+
+        pass
+
+
+    def delete_store_object(self, id):
+
+        """
+        Delete a store object
+
+        :param id: The object id.
+        """
+
+        pass
