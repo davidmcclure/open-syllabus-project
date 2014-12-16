b072b492114e357294438c4fdbbaf19d0c129cb0
David McClure <davidwilliammcclure@gmail.com>
2014-12-16 15:17:31 -0800

    Sketches in a write_document_objects command for osp locations.

--- bin/commands/locations.py
+++ bin/commands/locations.py
@@ -5,11 +5,13 @@ import click
 import csv
 
 from osp.common.models.base import database
+from osp.common.overview import Overview
 from osp.locations.models.doc_to_inst import DocToInst
 from osp.locations.jobs.locate import locate
 from osp.institutions.models.institution import Institution
 from osp.institutions.models.lonlat import LonLat
 from osp.corpus.corpus import Corpus
+from osp.corpus.models.document import Document
 from rq import Queue
 from redis import StrictRedis
 from peewee import *
@@ -43,6 +45,31 @@ def queue_location():
 
     for syllabus in Corpus.from_env().syllabi():
         queue.enqueue(locate, syllabus.path)
+
+
+@cli.command()
+@click.option('--page', default=50)
+def write_document_objects(page):
+
+    """
+    DEV: Write document objects into Overview.
+    """
+
+    ov = Overview.from_env()
+
+    # Select the current values.
+    query = DocToInst.select_current()
+
+    objects = []
+    for d2i in query.naive().iterator():
+
+        # Get the document.
+        doc = Document.get(Document.path==d2i.document)
+        objects.append([doc.id, d2i.institution.id])
+
+    # Write the objects in pages.
+    for i in range(0, len(objects), page):
+        ov.post_document_object(objects[i:i+page])
 
 
 @cli.command()
