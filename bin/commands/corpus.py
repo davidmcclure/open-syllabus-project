123a96e774acb500106e7a2d3b0cb36fea0e7e41
David McClure <davidwilliammcclure@gmail.com>
2014-12-16 09:28:23 -0800

    Adds clint to the build.

--- bin/commands/corpus.py
+++ bin/commands/corpus.py
@@ -1,9 +1,13 @@
 
 
+import os
 import click
 
 from osp.common.models.base import database
 from osp.corpus.models.document import Document
+from osp.corpus.corpus import Corpus
+from rq import Queue
+from redis import StrictRedis
 
 
 @click.group()
@@ -29,4 +33,8 @@ def queue_document_registration():
     Queue jobs to insert documents in the database.
     """
 
-    pass
+    # TODO: Make env-configurable.
+    queue = Queue('osp-corpus', connection=StrictRedis())
+    corpus = Corpus(os.environ['OSP_CORPUS'])
+
+    print(corpus.file_count)
