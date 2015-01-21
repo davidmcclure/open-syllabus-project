250fe33c2f732b853eac8f3cf281962b95770aa5
David McClure <davidwilliammcclure@gmail.com>
2015-01-21 14:06:42 -0800

    Implements an 'osp dates queue' command.

--- bin/commands/dates.py
+++ bin/commands/dates.py
@@ -1,8 +1,10 @@
 
 
 import click
+import math
 
 from osp.common.models.base import postgres, redis
+from osp.corpus.models.text import Document_Text
 from osp.dates.semester.models.semester import Document_Semester
 from osp.dates.semester.jobs.ext_semester import ext_semester
 from rq import Queue
@@ -28,10 +30,29 @@ def init_db():
 
 
 @cli.command()
-def queue_semester_extraction():
+@click.option('--n', default=10000)
+def queue(n):
 
     """
     Queue semester extraction queries.
+    TODO: Break out the page-through-all-records logic.
     """
 
-    pass
+    queue = Queue(connection=redis)
+
+    query = (
+        Document_Text
+        .select()
+        .distinct(Document_Text.document)
+        .order_by(
+            Document_Text.document,
+            Document_Text.created.desc()
+        )
+    )
+
+    # Get the number of pages.
+    page_count = math.ceil(query.count()/n)
+
+    for page in range(page_count):
+        for text in query.paginate(page, n).naive().iterator():
+            queue.enqueue(ext_semester, text.document)
--- osp/dates/semester/jobs/ext_semester.py
+++ osp/dates/semester/jobs/ext_semester.py
@@ -20,7 +20,7 @@ def ext_semester(path):
         (?P<semester>fall|winter|spring|summer)
         \s+
         (?P<year>\d{4}|\d{2})
-        ''', re.I+re.X)
+    ''', re.I+re.X)
 
     matches = [m.groupdict() for m in re.finditer(pattern, text)]
 
