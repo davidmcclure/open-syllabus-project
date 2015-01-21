b26d8e6a45d8277b6176891e07003d4208955c82
David McClure <davidwilliammcclure@gmail.com>
2015-01-21 11:02:29 -0800

    Stub for the semester extractor job.

---
+++ osp/dates/semester/jobs/ext_semester.py
@@ -0,0 +1,14 @@
+
+
+from osp.dates.semester.models.semester import Document_Semester
+
+
+def ext_semester(path):
+
+    """
+    Extract a "Fall|Spring YYYY" semester.
+
+    :param str path: The document path.
+    """
+
+    pass
