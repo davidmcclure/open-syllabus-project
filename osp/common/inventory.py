e6c1cc371814139aca2ad61c811fa70d6180615f
David McClure <davidwilliammcclure@gmail.com>
2015-01-23 14:01:00 -0800

    Adds an osp.common.inventory module, which gets the list of OSP worker IPs.

---
+++ osp/common/inventory.py
@@ -0,0 +1,33 @@
+
+
+import os
+
+from boto import ec2
+
+
+class Inventory:
+
+
+    def __init__(self):
+
+        """
+        Initialize the EC2 connection.
+        """
+
+        self.conn = ec2.connect_to_region(os.environ['OSP_REGION'])
+
+
+    @property
+    def worker_ips(self):
+
+        """
+        Get a list of worker IP addresses.
+        """
+
+        ips = []
+        for r in self.conn.get_all_reservations():
+            for i in r.instances:
+                if i.tags.get('osp', False) == 'worker':
+                    ips.append(i.ip_address)
+
+        return ips
