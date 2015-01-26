26e37c65ca2f6cb1f9098bfa2f18a4dbd2efbc44
David McClure <davidwilliammcclure@gmail.com>
2015-01-26 09:11:08 -0800

    Moves the queue_read_text() task into the job file, alongside read_text.

--- osp/corpus/jobs/read_text.py
+++ osp/corpus/jobs/read_text.py
@@ -1,7 +1,10 @@
 
 
+from osp.common.models.base import redis
 from osp.corpus.syllabus import Syllabus
 from osp.corpus.models.text import Document_Text
+from osp.corpus.corpus import Corpus
+from rq import Queue
 
 
 def read_text(path):
@@ -20,3 +23,18 @@ def read_text(path):
             text=syllabus.unbroken_text,
             document=syllabus.relative_path
         )
+
+
+def queue_read_text(s1, s2):
+
+    """
+    Queue text extraction tasks in the worker.
+
+    :param s1: The first segment.
+    :param s2: The last segment.
+    """
+
+    queue = Queue(connection=redis)
+
+    for syllabus in Corpus.from_env(s1=s1, s2=s2).syllabi():
+        queue.enqueue(read_text, syllabus.path)
