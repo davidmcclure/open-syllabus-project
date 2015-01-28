3f4bc39511089b80fdcdd85283625ce3ee8d93fb
David McClure <davidwilliammcclure@gmail.com>
2015-01-27 17:28:41 -0800

    Moves the logic to queue the meta job-queuing-job into the Flask callback.

--- osp/corpus/api.py
+++ osp/corpus/api.py
@@ -1,9 +1,9 @@
 
 
-import threading
-
-from osp.corpus.jobs.read_text import queue
+from osp.common.models.base import redis
+from osp.corpus.jobs.read_text import queue_read_text
 from flask import Flask, Blueprint, request
+from rq import Queue
 
 
 corpus = Blueprint('corpus', __name__)
@@ -14,5 +14,9 @@ def test():
 
     s1 = int(request.args['s1'])
     s2 = int(request.args['s2'])
-    queue(s1, s2)
+
+    queue = Queue(connection=redis)
+    job = queue.enqueue(queue_read_text, s1, s2, timeout=3600)
+
+    code = 200 if job.is_queued else 500
     return ('', 200)
