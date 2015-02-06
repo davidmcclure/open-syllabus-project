8172814bbe68e439f387dfa8c751b0c50d1ac115
David McClure <davidwilliammcclure@gmail.com>
2015-02-06 14:02:48 -0800

    Adds a standalone queue_locate function, which can be used by a REST API for the location matching.

--- bin/commands/locations.py
+++ bin/commands/locations.py
@@ -7,13 +7,12 @@ import csv
 from osp.common.models.base import pg_worker, redis
 from osp.common.overview import Overview
 from osp.locations.models.doc_inst import Document_Institution
-from osp.locations.jobs.locate import locate
+from osp.locations.jobs.locate import queue_locate
 from osp.locations import queries
 from osp.institutions.models.institution import Institution
 from osp.corpus.corpus import Corpus
 from osp.corpus.models.document import Document
 from rq import Queue
-from redis import StrictRedis
 from peewee import *
 
 
@@ -44,9 +43,7 @@ def queue_location_matching():
     """
 
     queue = Queue(connection=redis)
-
-    for syllabus in Corpus.from_env().cli_syllabi():
-        queue.enqueue(locate, syllabus.path)
+    queue.enqueue(queue_locate)
 
 
 @cli.command()
--- osp/locations/jobs/locate.py
+++ osp/locations/jobs/locate.py
@@ -1,8 +1,11 @@
 
 
+from osp.common.models.base import redis
 from osp.corpus.syllabus import Syllabus
+from osp.corpus.corpus import Corpus
 from osp.institutions.models.institution import Institution
 from osp.locations.models.doc_inst import Document_Institution
+from rq import Queue
 
 
 def locate(path):
@@ -36,3 +39,18 @@ def locate(path):
             institution=match,
             document=syllabus.relative_path
         )
+
+
+def queue_locate(s1=0, s2=4095):
+
+    """
+    Queue text extraction tasks in the worker.
+
+    :param s1: The first segment.
+    :param s2: The last segment.
+    """
+
+    queue = Queue(connection=redis)
+
+    for syllabus in Corpus.from_env(s1=s1, s2=s2).syllabi():
+        queue.enqueue(locate, syllabus.path)
