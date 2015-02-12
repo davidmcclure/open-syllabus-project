3ed43385fb45d17a0bb569b9eb6a321eac8dde92
David McClure <davidwilliammcclure@gmail.com>
2015-02-12 10:06:56 -0800

    Makes it possible to limit the number of rows that get evaluated in the department matching command.

--- bin/commands/subjects.py
+++ bin/commands/subjects.py
@@ -4,7 +4,6 @@ import csv
 import click
 import re
 
-from osp.common.utils import paginate_query
 from osp.corpus.queries import all_document_texts
 from collections import Counter
 
@@ -16,9 +15,9 @@ def cli():
 
 @cli.command()
 @click.argument('out_path', type=click.Path())
-@click.option('--page_len', default=1000)
+@click.option('--limit', default=1000)
 @click.option('--skim_depth', default=500)
-def csv_depts(out_path, page_len, skim_depth):
+def csv_depts(out_path, limit, skim_depth):
 
     """
     Extract "course number" strings to a file.
@@ -31,23 +30,22 @@ def csv_depts(out_path, page_len, skim_depth):
     writer = csv.DictWriter(out_file, cols)
     writer.writeheader()
 
-    # Paginate the text table.
-    query = all_document_texts()
-    pages = paginate_query(query, page_len, bar=True)
+    # Limit the text table.
+    query = all_document_texts().limit(limit)
 
     # Match things like "PHYS 101".
     regex = re.compile('(?P<dept>[A-Z]{2,})\s+[0-9]{2,}')
+
     depts = Counter()
+    for doc in query.iterator():
 
-    for page in pages:
-        for doc in page.iterator():
+        # Take the first N chars.
+        frag = doc.text[:skim_depth]
 
-            # Take the first N chars.
-            frag = doc.text[:skim_depth]
+        # Write the matches to a file.
+        for m in re.finditer(regex, frag):
+            depts[m.group('dept')] += 1
 
-            # Write the matches to a file.
-            for m in re.finditer(regex, frag):
-                depts[m.group('dept')] += 1
-
+    # Write the CSV.
     for d in depts.most_common():
         writer.writerow({'dept': d[0], 'count': d[1]})
