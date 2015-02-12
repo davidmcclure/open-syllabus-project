cb623060c9430a84bd557a9f1658fdd0bb20f2f6
David McClure <davidwilliammcclure@gmail.com>
2015-02-12 08:58:17 -0800

    Committing the 'subjects' CLI suite.

---
+++ bin/commands/subjects.py
@@ -0,0 +1,38 @@
+
+
+import click
+import re
+
+from osp.corpus.queries import all_document_texts
+from osp.common.utils import paginate_query
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('out_path', type=click.Path())
+@click.option('--page_len', default=1000)
+@click.option('--skim_depth', default=500)
+def course_numbers(out_path, page_len, skim_depth):
+
+    """
+    Extract "course number" strings to a file.
+    """
+
+    query = all_document_texts()
+    pages = paginate_query(query, page_len, bar=True)
+    regex = re.compile('[A-Z]{2,}\s+[0-9]{2,}')
+    out_file = open(out_path, 'w')
+
+    for page in pages:
+        for doc in page.iterator():
+
+            # Take the first N chars.
+            frag = doc.text[:skim_depth]
+
+            # Write the matches to a file.
+            for match in re.findall(regex, frag):
+                print(match, file=out_file)
