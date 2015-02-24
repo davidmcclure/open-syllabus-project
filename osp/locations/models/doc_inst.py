24a6cbe1c0ed89d0c8ec51cf32571757f54b55c8
David McClure <davidwilliammcclure@gmail.com>
2015-02-24 15:47:33 -0800

    In the document-institution table, identify the document with a FK reference.

--- osp/locations/models/doc_inst.py
+++ osp/locations/models/doc_inst.py
@@ -4,11 +4,12 @@ import datetime
 
 from osp.common.models.base import RemoteModel
 from osp.institutions.models.institution import Institution
+from osp.corpus.models.document import Document
 from peewee import *
 
 
 class Document_Institution(RemoteModel):
 
     created = DateTimeField(default=datetime.datetime.now)
-    document = CharField(index=True)
+    document = ForeignKeyField(Document)
     institution = ForeignKeyField(Institution)
