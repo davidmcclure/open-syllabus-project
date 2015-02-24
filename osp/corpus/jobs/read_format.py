1a6670ededf5b21d9c7f2f0abe21c7e74b31d8bc
David McClure <davidwilliammcclure@gmail.com>
2015-02-24 14:24:02 -0800

    Updates the read_format job so that it takes a document row id, instead of a physical doc path.

--- osp/corpus/jobs/read_format.py
+++ osp/corpus/jobs/read_format.py
@@ -1,20 +1,21 @@
 
 
-from osp.corpus.syllabus import Syllabus
+from osp.corpus.models.document import Document
 from osp.corpus.models.format import Document_Format
+from osp.corpus.syllabus import Syllabus
 
 
-def read_format(path):
+def read_format(id):
 
     """
     Write the libmagic file format.
 
-    :param str path: The document path.
+    :param id: The document id.
     """
 
-    syllabus = Syllabus(path)
+    doc = Document.select(Document.id==id)
 
     Document_Format.create(
-        format=syllabus.libmagic_file_type,
+        format=doc.syllabus.libmagic_file_type,
         document=syllabus.relative_path
     )
--- osp/corpus/models/document.py
+++ osp/corpus/models/document.py
@@ -1,5 +1,6 @@
 
 
+from osp.corpus.syllabus import Syllabus
 from playhouse.postgres_ext import *
 from osp.common.models.base import LocalModel
 from peewee import *
@@ -7,5 +8,16 @@ from peewee import *
 
 class Document(LocalModel):
 
+
     stored_id = BigIntegerField(null=True)
     path = CharField(unique=True)
+
+
+    @property
+    def syllabus(self):
+
+        """
+        Wrap the row as an OSP Syllabus instance.
+        """
+
+        pass
