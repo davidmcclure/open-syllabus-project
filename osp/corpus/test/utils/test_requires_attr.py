6deb8f1f280c266102d30eeda6595d289b5fe387
David McClure <davidwilliammcclure@gmail.com>
2015-02-26 09:03:59 -0800

    Adds tests for the requires_attr helper.

---
+++ osp/corpus/test/utils/test_requires_attr.py
@@ -0,0 +1,26 @@
+
+
+import pytest
+
+from osp.corpus.utils import requires_attr
+
+
+class Test:
+    @requires_attr('attr')
+    def needs_attr(self):
+        return True
+
+
+@pytest.fixture
+def instance():
+    return Test()
+
+
+def test_attr_exists(instance):
+    assert instance.needs_attr() == None
+
+
+def test_attr_does_not_exist(instance):
+    instance.attr = True
+    assert instance.needs_attr() == True
+
