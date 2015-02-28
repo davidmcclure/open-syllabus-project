394cc68e2732b02868d6aaa191dc63c0d849558a
David McClure <davidwilliammcclure@gmail.com>
2015-02-27 19:36:14 -0800

    Tests for Corpus#segments() with unconstrained partitions.

--- osp/corpus/test/corpus/test_segments.py
+++ osp/corpus/test/corpus/test_segments.py
@@ -1,15 +1,30 @@
 
 
+import os
+
 from osp.corpus.segment import Segment
 from osp.corpus.corpus import Corpus
+from osp.corpus.utils import int_to_dir
 
 
 def test_full_partition(mock_corpus):
 
     """
-    When no segments are passed to Corpus#segments(), instances for all 4096
-    segments should be generated.
+    When no segment boundaries are passed to Corpus#segments(), instances for
+    all 4096 segments should be generated.
     """
 
-    # All all segments.
+    # Add all segments.
     mock_corpus.add_segments()
+    corpus = Corpus(mock_corpus.path)
+    segments = corpus.segments()
+
+    for i in range(0, 4095):
+
+        # Should be a Segment instance.
+        segment = next(segments)
+        assert isinstance(segment, Segment)
+
+        # Should have the right path.
+        dpath = os.path.join(corpus.path, int_to_dir(i))
+        assert segment.path == dpath
