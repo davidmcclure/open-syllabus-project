51bffd78df05a4bc65354108dadf055dd8df8322
David McClure <davidwilliammcclure@gmail.com>
2015-03-02 10:43:57 -0800

    Tests the happy case for the grouper() helper.

--- osp/common/test/utils/test_grouper.py
+++ osp/common/test/utils/test_grouper.py
@@ -6,7 +6,15 @@ from osp.common.utils import grouper
 def test_even_groups():
 
     """
-    TODO
+    When the total size of the iterable is evenly divided by the group size,
+    all groups should be generated.
     """
 
-    assert True == True
+    groups = grouper(range(100), 10)
+
+    # Should generate 10 groups, each of length 10.
+    for i in range(10):
+        assert list(next(groups)) == list(range(i*10, (i*10)+10))
+
+    # And then stop.
+    assert next(groups, False) == False
