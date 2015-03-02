5606fa29a8fe56615236427ae9efb7d929904235
David McClure <davidwilliammcclure@gmail.com>
2015-03-02 12:49:14 -0800

    In the config module, use a Config class instead of just a raw anyconfig instance, which will make it easier to implement the database configuration loading.

--- osp/common/config.py
+++ osp/common/config.py
@@ -4,22 +4,63 @@ import os
 import anyconfig
 
 
-files = [
-
-    # Defaults first.
-    os.path.join(os.path.dirname(__file__), 'osp.yml'),
-
-    # Custom configs.
-    '/etc/osp/osp.yml',
-    '~/osp.yml',
-    './osp.yml'
-
-]
-
+# Throttle the logging.
 anyconfig.set_loglevel('WARNING')
 
-config = anyconfig.load(
-    files,
-    merge=anyconfig.MS_DICTS,
-    ignore_missing=True
-)
+
+class Config:
+
+
+    @classmethod
+    def from_env(cls):
+
+        """
+        Get a config instance with the default file precedence.
+        """
+
+        return cls([
+
+            # Defaults first.
+            os.path.join(os.path.dirname(__file__), 'osp.yml'),
+
+            # Custom configs.
+            '/etc/osp/osp.yml',
+            '~/osp.yml',
+            './osp.yml'
+
+        ])
+
+
+    def __init__(self, paths):
+
+        """
+        Initialize the configuration object.
+
+        Args:
+            paths (list): YAML paths, from the most to least specific.
+        """
+
+        self.config = anyconfig.load(
+            paths,
+            merge=anyconfig.MS_DICTS,
+            ignore_missing=True
+        )
+
+
+    def __getitem__(self, key):
+
+        """
+        Get a configuration value.
+
+        Args:
+            key (str): The configuration key.
+
+        Returns:
+            The option value.
+        """
+
+        return self.config[key]
+
+
+# Global instance.
+config = Config.from_env()
