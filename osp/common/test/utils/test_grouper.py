91db256aad26af1d8ddda4234cd2f0d2a38deffe
David McClure <davidwilliammcclure@gmail.com>
2015-03-02 10:54:25 -0800

    Test uneven group division in the grouper() helper.

--- osp/common/test/utils/test_grouper.py
+++ osp/common/test/utils/test_grouper.py
@@ -18,3 +18,21 @@ def test_even_groups():
 
     # And then stop.
     assert next(groups, False) == False
+
+
+def test_uneven_groups():
+
+    """
+    When the group size doesn't evenly divide the iterable, all full-sized
+    groups should be yielded, followed by a final group with the remainder.
+    """
+
+    groups = grouper(range(105), 10)
+
+    # Should generate the 10 full-sized groups.
+    for i in range(10):
+        assert list(next(groups)) == list(range(i*10, (i*10)+10))
+
+    # And then the last 5 elements.
+    assert list(next(groups)) == list(range(100, 105))
+    assert next(groups, False) == False
