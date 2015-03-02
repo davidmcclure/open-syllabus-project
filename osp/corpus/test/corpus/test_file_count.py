1aa7cf4e69f97377f5c67791b383f3b60184a69a
David McClure <davidwilliammcclure@gmail.com>
2015-03-02 09:03:08 -0800

    Stubs in a Corpus#file_count test, adds __init__.py's to pytest directories.

---
+++ osp/corpus/test/corpus/test_file_count.py
@@ -0,0 +1,12 @@
+
+
+from osp.corpus.corpus import Corpus
+
+
+def test_file_count(mock_corpus):
+
+    """
+    Corpus#file_count should return the number of files in all segments.
+    """
+
+    pass
