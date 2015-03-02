463ef638148e280fed1bd1eaa3a9078f9f1003db
David McClure <davidwilliammcclure@gmail.com>
2015-03-02 14:09:22 -0800

    Refactors the get_config() helper so that it can take a variable number of fixture file names.

--- osp/common/test/config/conftest.py
+++ osp/common/test/config/conftest.py
@@ -6,7 +6,25 @@ import pytest
 from osp.common.config import Config
 
 
-def get_config(name):
+def get_path(name):
+
+    """
+    Get the path of a config file in /fixtures.
+
+    Args:
+        name (str): The (base) file name.
+
+    Returns:
+        str: The file path.
+    """
+
+    return os.path.join(
+        os.path.dirname(__file__),
+        'fixtures/'+name+'.yml'
+    )
+
+
+def get_config(*names):
 
     """
     Load a fixture config file from /fixtures.
@@ -18,9 +36,5 @@ def get_config(name):
         Config: The config instance.
     """
 
-    path = os.path.join(
-        os.path.dirname(__file__),
-        'fixtures/'+name+'.yml'
-    )
-
-    return Config([path])
+    paths = list(map(get_path, names))
+    return Config(paths)
---
+++ osp/common/test/config/test_merge.py
@@ -0,0 +1,15 @@
+
+
+import os
+
+from .conftest import get_config
+
+
+def test_merge():
+
+    """
+    When a config object is instantiated with multiple files, the contents of
+    the files should be merged together.
+    """
+
+    pass
