0e88c0e749e8d2122d4f567275f5ce49c3465035
David McClure <davidwilliammcclure@gmail.com>
2015-03-03 10:06:46 -0800

    Blocking in the beginning of a Document.insert_documents() test.

--- osp/corpus/test/models/document/test_insert_documents.py
+++ osp/corpus/test/models/document/test_insert_documents.py
@@ -1,12 +1,29 @@
 
 
+from osp.corpus.corpus import Corpus
 from osp.corpus.models.document import Document
+from osp.corpus.utils import int_to_dir
 
 
-def test_insert_documents():
+def test_insert_documents(mock_corpus):
 
     """
     Corpus.insert_documents()
     """
 
-    pass
+    # add segments
+    # add files in segments
+    # insert docs
+    # assert rows
+
+    # Add 10 segments.
+    mock_corpus.add_segments(s1=0, s2=10)
+
+    # Add 10 files per segment.
+    for i in range(0, 10):
+        segment = int_to_dir(i)
+        mock_corpus.add_files(segment, 10, prefix=segment+'-')
+
+    # Insert document rows.
+    corpus = Corpus(mock_corpus.path)
+    Document.insert_documents(corpus)
