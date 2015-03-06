899f7aac5f41b7520de0208933eee03343569311
David McClure <davidwilliammcclure@gmail.com>
2015-03-06 10:21:56 -0500

    Blocking in the basic structure for read_text() tests.

---
+++ osp/test/corpus/jobs/test_read_text.py
@@ -0,0 +1,17 @@
+
+
+from osp.corpus.jobs.read_text import read_text
+
+
+def test_text_extraction_succeeds(Document, mock_corpus):
+
+    """
+    read_text() should extract text for a document and write the result into
+    the `document_text` table.
+    """
+
+    # Add a file, create a document row.
+    path = mock_corpus.add_file(content='text')
+    document = Document.create(path=path)
+
+    read_text(document.id)
