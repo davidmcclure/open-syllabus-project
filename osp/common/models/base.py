f6b53af04ba4b609a5af99369574dc46f2bb8c49
David McClure <davidwilliammcclure@gmail.com>
2015-03-06 22:54:43 -0500

    Adds a max_id() method to the base Postgres class.

--- osp/common/models/base.py
+++ osp/common/models/base.py
@@ -4,9 +4,9 @@ import logging
 import datetime
 
 from osp.common.config import config
+from peewee import Model, DateTimeField, fn
 from redis import StrictRedis
 from elasticsearch import Elasticsearch
-from peewee import Model, DateTimeField
 
 
 # REDIS
@@ -22,4 +22,16 @@ logging.getLogger('elasticsearch.trace').propagate = False
 
 
 class BaseModel(Model):
+
+
     created = DateTimeField(default=datetime.datetime.now)
+
+
+    @classmethod
+    def max_id(cls):
+
+        """
+        Get the highest id in the table.
+        """
+
+        return cls.select(fn.max(cls.id)).scalar()
