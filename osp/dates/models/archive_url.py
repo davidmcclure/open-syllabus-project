6371174e73a54a2abee33d9c8b6f4aa84dbaf9f0
David McClure <davidwilliammcclure@gmail.com>
2015-03-07 13:30:35 -0500

    Adds a document_date_archive_url table.

---
+++ osp/dates/models/archive_url.py
@@ -0,0 +1,17 @@
+
+
+from osp.common.config import config
+from osp.common.models.base import BaseModel
+from osp.corpus.models.document import Document
+from peewee import *
+
+
+class Document_Date_Archive_Url(BaseModel):
+
+
+    document = ForeignKeyField(Document, unique=True)
+    timestamp = CharField(index=True)
+
+
+    class Meta:
+        database = config.get_table_db('document_date_archive_url')
