91e6ac425fcf799b9912f5158965657fdbe55c34
David McClure <davidwilliammcclure@gmail.com>
2015-03-12 13:30:47 -0700

    Scaffolding for the file metadata date extractor.

---
+++ osp/dates/jobs/ext_file_metadata.py
@@ -0,0 +1,20 @@
+
+
+import re
+
+from osp.corpus.models.document import Document
+from osp.dates.models.file_metadata import Document_Date_File_Metadata
+from datetime import datetime
+
+
+def ext_file_metadata(id):
+
+    """
+    Try to extract a created date from PDF and DOCX file metadata.
+
+    Args:
+        id (int): The document id.
+    """
+
+    doc = Document.get(Document.id==id)
+    # TODO
---
+++ osp/test/dates/jobs/test_ext_file_metadata.py
@@ -0,0 +1,14 @@
+
+
+from osp.corpus.models.document import Document
+from osp.dates.models.file_metadata import Document_Date_File_Metadata
+from osp.corpus.jobs.ext_file_metadata import ext_file_metadata
+
+
+def test_test():
+
+    """
+    TODO
+    """
+
+    pass
