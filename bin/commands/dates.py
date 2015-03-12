d7f2c5e87083d077a932799afd9405d3d91952a8
David McClure <davidwilliammcclure@gmail.com>
2015-03-12 13:32:32 -0700

    In the dates CLI suite, create the file-metadata table.

--- bin/commands/dates.py
+++ bin/commands/dates.py
@@ -6,6 +6,7 @@ from osp.common.utils import query_bar
 from osp.corpus.models.document import Document
 from osp.dates.models.archive_url import Document_Date_Archive_Url
 from osp.dates.models.semester import Document_Date_Semester
+from osp.dates.models.file_metadata import Document_Date_File_Metadata
 from osp.dates.jobs.ext_archive_url import ext_archive_url
 from osp.dates.jobs.ext_semester import ext_semester
 from peewee import create_model_tables
@@ -26,7 +27,8 @@ def init_db():
 
     create_model_tables([
         Document_Date_Archive_Url,
-        Document_Date_Semester
+        Document_Date_Semester,
+        Document_Date_File_Metadata
     ], fail_silently=True)
 
 
