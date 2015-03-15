460d6a046934d8b14e81fc71e6fba20007c93695
David McClure <davidwilliammcclure@gmail.com>
2015-03-15 00:29:09 -0700

    Adds an ext() fixture helper in the archive URL tests.

--- osp/test/dates/jobs/test_ext_archive_url.py
+++ osp/test/dates/jobs/test_ext_archive_url.py
@@ -1,5 +1,7 @@
 
 
+import pytest
+
 from osp.corpus.models.document import Document
 from osp.dates.models.archive_url import Document_Date_Archive_Url
 from osp.dates.jobs.ext_archive_url import date_format, ext_archive_url
@@ -7,7 +9,39 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
-def test_archive_url(models, mock_corpus):
+@pytest.fixture()
+def ext(models, mock_corpus):
+
+    """
+    Provide a function that mocks a file with a given URL and returns a
+    document instance bound to the new path.
+
+    Returns:
+        function: A helper that mocks a file with the provided URL, runs the
+        job, and returns the new `document_date_archive_url` row.
+    """
+
+    def _ext(url):
+
+        # Create a document.
+        path = mock_corpus.add_file(log={'url': url})
+        document = Document.create(path=path)
+
+        # Extract the date.
+        ext_archive_url(document.id)
+
+        # Pop out the new row.
+        return (
+            Document_Date_Archive_Url
+            .select()
+            .where(Document_Date_Archive_Url.document==document)
+            .first()
+        )
+
+    return _ext
+
+
+def test_archive_url(ext):
 
     """
     ext_archive_url() should extract a timestamp from an Internet Archive URL.
@@ -16,19 +50,7 @@ def test_archive_url(models, mock_corpus):
     url1 = 'https://web.archive.org/web/20150102030405'
     url2 = 'http://yale.edu/syllabus.html'
 
-    # Internet Archive URL.
-    path = mock_corpus.add_file(log={
-        'url': url1+'/'+url2
-    })
-
-    # Write the timestamp.
-    document = Document.create(path=path)
-    ext_archive_url(document.id)
-
-    # Pop out the new row.
-    row = Document_Date_Archive_Url.get(
-        Document_Date_Archive_Url.document==document
-    )
+    row = ext(url1+'/'+url2)
 
     assert row.date.year    == 2015
     assert row.date.month   == 1
@@ -38,7 +60,7 @@ def test_archive_url(models, mock_corpus):
     assert row.date.second  == 5
 
 
-def test_ignore_future_timestamp(models, mock_corpus):
+def test_ignore_future_timestamp(ext):
 
     """
     Don't index timestamps from the future.
@@ -51,33 +73,19 @@ def test_ignore_future_timestamp(models, mock_corpus):
     url1 = 'https://web.archive.org/web/'+timestamp
     url2 = 'http://yale.edu/syllabus.html'
 
-    # Timestamp in the future.
-    path = mock_corpus.add_file(log={
-        'url': url1+'/'+url2
-    })
-
-    # Write the timestamp.
-    document = Document.create(path=path)
-    ext_archive_url(document.id)
+    row = ext(url1+'/'+url2)
 
     # Shouldn't write a row.
     assert Document_Date_Archive_Url.select().count() == 0
 
 
-def test_ignore_regular_url(models, mock_corpus):
+def test_ignore_regular_url(ext):
 
     """
     When the syllabus was scraped from a regular URL, don't write a row.
     """
 
-    # Regular URL.
-    path = mock_corpus.add_file(log={
-        'url': 'http://yale.edu/syllabus.html'
-    })
-
-    # Write the timestamp.
-    document = Document.create(path=path)
-    ext_archive_url(document.id)
+    row = ext('http://yale.edu/syllabus.html')
 
     # Shouldn't write a row.
     assert Document_Date_Archive_Url.select().count() == 0
