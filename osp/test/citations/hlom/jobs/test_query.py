74c81f2e10f8999f43e3419ba59b5c2e1a6c508d
David McClure <davidwilliammcclure@gmail.com>
2015-03-20 11:29:21 -0700

    When no rows match an HLOM query, test that no link rows are written.

--- osp/test/citations/hlom/jobs/test_query.py
+++ osp/test/citations/hlom/jobs/test_query.py
@@ -1,5 +1,7 @@
 
 
+import pytest
+
 from osp.corpus.models.document import Document
 from osp.corpus.jobs.ext_text import ext_text
 from osp.citations.hlom.models.record import HLOM_Record
@@ -8,10 +10,10 @@ from osp.citations.hlom.jobs.query import query
 from pymarc import Record, Field
 
 
-def get_marc(number, title, author):
+def get_hlom(number, title, author):
 
     """
-    Create a MARC record.
+    Insert a HLOM record row.
 
     Args:
         number (str): The control number.
@@ -22,7 +24,7 @@ def get_marc(number, title, author):
         pymarc.Record
     """
 
-    record = Record()
+    marc = Record()
 
     f001 = Field(
         tag='001',
@@ -42,53 +44,82 @@ def get_marc(number, title, author):
         subfields=['a', title]
     )
 
-    record.add_field(f001)
-    record.add_field(f100)
-    record.add_field(f245)
+    marc.add_field(f001)
+    marc.add_field(f100)
+    marc.add_field(f245)
 
-    return record
+    return HLOM_Record.create(
+        control_number=number,
+        record=marc.as_marc()
+    )
 
 
-def test_matches(models, mock_osp, corpus_index):
+@pytest.fixture()
+def doc(models, mock_osp):
+
+    """
+    Mocks a file, create a document row, and extract text.
+
+    Returns:
+        function
+    """
+
+    def _doc(content):
+
+        # Mock a file.
+        path = mock_osp.add_file(content=content)
+
+        # Create a `document` row.
+        document = Document.create(path=path)
+
+        # Extract text.
+        text = ext_text(document.id)
+
+        return document
+
+    return _doc
+
+
+def test_matches(doc, corpus_index):
 
     """
     When OSP documents match the query, write link rows.
     """
 
-    p1 = mock_osp.add_file(content='War and Peace, Leo Tolstoy 1')
-    p2 = mock_osp.add_file(content='War and Peace, Leo Tolstoy 2')
-    p3 = mock_osp.add_file(content='War and Peace, Leo Tolstoy 3')
-    p4 = mock_osp.add_file(content='Anna Karenina, Leo Tolstoy 1')
-    p5 = mock_osp.add_file(content='Anna Karenina, Leo Tolstoy 2')
-
-    d1 = Document.create(path=p1)
-    d2 = Document.create(path=p2)
-    d3 = Document.create(path=p3)
-    d4 = Document.create(path=p4)
-    d5 = Document.create(path=p5)
-
-    t1 = ext_text(d1.id)
-    t2 = ext_text(d2.id)
-    t3 = ext_text(d3.id)
-    t4 = ext_text(d4.id)
-    t5 = ext_text(d5.id)
+    d1 = doc('War and Peace, Leo Tolstoy 1')
+    d2 = doc('War and Peace, Leo Tolstoy 2')
+    d3 = doc('War and Peace, Leo Tolstoy 3')
+    d4 = doc('Anna Karenina, Leo Tolstoy 1')
+    d5 = doc('Anna Karenina, Leo Tolstoy 2')
 
     corpus_index.index()
 
-    marc = get_marc('1', 'War and Peace', 'Leo Tolstoy')
+    hlom = get_hlom('1', 'War and Peace', 'Leo Tolstoy')
+    query(hlom.id)
 
-    record = HLOM_Record.create(
-        control_number='1',
-        record=marc.as_marc()
-    )
-
-    query(record.id)
-
+    # Should write 3 citation links.
     assert HLOM_Citation.select().count() == 3
 
+    # Should match the right documents.
     for doc in [d1, d2, d3]:
 
         assert HLOM_Citation.select().where(
             HLOM_Citation.document==doc,
-            HLOM_Citation.record==record
+            HLOM_Citation.record==hlom
         )
+
+
+def test_no_matches(doc, corpus_index):
+
+    """
+    When no documents match, don't write any rows.
+    """
+
+    doc('War and Peace, Leo Tolstoy')
+    corpus_index.index()
+
+    hlom = get_hlom('1', 'Master and Man', 'Leo Tolstoy')
+    query(hlom.id)
+
+    # Shouldn't write any rows.
+    assert HLOM_Citation.select().count() == 0
