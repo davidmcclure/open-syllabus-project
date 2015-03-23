43c47fd374807fbcc05d75a88258453e31b62fae
David McClure <davidwilliammcclure@gmail.com>
2015-03-23 16:39:09 -0700

    Don't keep references to mock documents, when they aren't used.

--- osp/test/citations/hlom/models/citation/test_text_counts.py
+++ osp/test/citations/hlom/models/citation/test_text_counts.py
@@ -12,19 +12,19 @@ def test_text_counts(corpus_index, add_hlom, add_doc):
     """
 
     # 1 citation:
-    r1  = add_hlom('War and Peace', 'Leo Tolstoy')
-    d1  = add_doc('War and Peace, Leo Tolstoy')
+    r1 = add_hlom('War and Peace', 'Leo Tolstoy')
+    add_doc('War and Peace, Leo Tolstoy')
 
     # 2 citations:
-    r2  = add_hlom('Anna Karenina', 'Leo Tolstoy')
-    d2b = add_doc('Anna Karenina, Leo Tolstoy b')
-    d2a = add_doc('Anna Karenina, Leo Tolstoy a')
+    r2 = add_hlom('Anna Karenina', 'Leo Tolstoy')
+    add_doc('Anna Karenina, Leo Tolstoy b')
+    add_doc('Anna Karenina, Leo Tolstoy a')
 
     # 3 citations:
-    r3  = add_hlom('Master and Man', 'Leo Tolstoy')
-    d3a = add_doc('Master and Man, Leo Tolstoy a')
-    d3b = add_doc('Master and Man, Leo Tolstoy b')
-    d3c = add_doc('Master and Man, Leo Tolstoy c')
+    r3 = add_hlom('Master and Man', 'Leo Tolstoy')
+    add_doc('Master and Man, Leo Tolstoy a')
+    add_doc('Master and Man, Leo Tolstoy b')
+    add_doc('Master and Man, Leo Tolstoy c')
 
     corpus_index.index()
     query(r1.id)
