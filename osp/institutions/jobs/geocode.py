70aa119a284ce057015ae7d1f2e51076ea5d65bf
David McClure <davidwilliammcclure@gmail.com>
2015-03-28 10:35:47 -0700

    Revert "Merges the 'institutions' module into 'locations'."
    This reverts commit a7abd20dc3ebcc044dbb2bf7819f9d98316bbe60.

---
+++ osp/institutions/jobs/geocode.py
@@ -0,0 +1,29 @@
+
+
+import os
+
+from geopy.geocoders import Nominatim
+
+
+def geocode(iid, query):
+
+    """
+    Geocode a query, write the lon/lat to Postgres.
+
+    :param int iid: The institution id.
+    :param str query: The query for the geocoder.
+    """
+
+    coder = Nominatim()
+
+    # Geocode.
+    location = coder.geocode(query, timeout=10)
+
+    if location:
+
+        # Write the coordinate.
+        Institution_LonLat.create(
+            institution=iid,
+            lat=location.latitude,
+            lon=location.longitude
+        )
---
+++ osp/locations/jobs/locate.py
@@ -0,0 +1,56 @@
+
+
+from osp.corpus.syllabus import Syllabus
+from osp.corpus.corpus import Corpus
+from osp.corpus.models.document import Document
+from osp.institutions.models.institution import Institution
+from osp.locations.models.doc_inst import Document_Institution
+from rq import Queue
+
+
+def locate(id):
+
+    """
+    Find an institution with the same base URL as a document.
+
+    :param id: The document id.
+    """
+
+    doc = Document.get(Document.id==id)
+
+    # Break if no manifest.
+    if not doc.syllabus.registered_domain:
+        return
+
+    # Form the domain query.
+    q = '%'+doc.syllabus.registered_domain+'%'
+
+    match = (
+        Institution
+        .select()
+        .where(Institution.metadata['Institution_Web_Address'] ** (q))
+        .order_by(Institution.id)
+        .first()
+    )
+
+    if match:
+
+        Document_Institution.create(
+            institution=match,
+            document=doc.id
+        )
+
+
+def queue_locate(s1=0, s2=4095):
+
+    """
+    Queue text extraction tasks in the worker.
+
+    :param s1: The first segment.
+    :param s2: The last segment.
+    """
+
+    queue = Queue(connection=redis)
+
+    for syllabus in Corpus.from_env(s1=s1, s2=s2).syllabi():
+        queue.enqueue(locate, syllabus.path)
