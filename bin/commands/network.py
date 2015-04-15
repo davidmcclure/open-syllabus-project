493a53d55a3b0167af9963af21c0bbf93f524ef2
David McClure <davidwilliammcclure@gmail.com>
2015-04-15 15:11:38 -0700

    In the network render CLI method, just accept a --size option.

--- bin/commands/network.py
+++ bin/commands/network.py
@@ -13,14 +13,12 @@ def cli():
 @cli.command()
 @click.argument('in_file', type=click.Path())
 @click.argument('out_file', type=click.Path())
-@click.option('--scale', type=int, default=5)
-@click.option('--size', type=int, default=320000)
-@click.option('--font_size', type=int, default=14)
-def render(in_file, out_file, scale, size, font_size):
+@click.option('--size', type=int, default=10000)
+def render(in_file, out_file, size):
 
     """
     Render the network.
     """
 
     n = GephiNetwork.from_gexf(in_file)
-    n.render(out_file, scale=scale, size=size, font_size=font_size)
+    n.render(out_file, size=size)
