742280809d7cdd61cc593265eefc746d1b423e29
David McClure <davidwilliammcclure@gmail.com>
2015-05-11 00:56:16 -0700

    Adds a 'corpus_csv syllabus_refinement' command.

--- bin/commands/corpus_csv.py
+++ bin/commands/corpus_csv.py
@@ -79,14 +79,48 @@ def random(out_file, n):
 
 
 @cli.command()
+@click.argument('in_file', type=click.File('r'))
 @click.argument('out_file', type=click.File('w'))
-@click.option('--n', default=10000)
+@click.option('--r', default=250)
 @click.option('--threshold', default=0.43)
-def syllabus_refinement(out_path, n, threshold):
+def syllabus_refinement(in_file, out_file, r, threshold):
 
     """
     Select the N documents around a given threshold in the
     syllabus/not-syllabus classifier predictions.
     """
 
-    pass
+    cols = ['path', 'score']
+    reader = csv.DictReader(in_file, cols)
+
+    # Gather ordered (path, score) tuples.
+    scores = [(r['path'], float(r['score'])) for r in reader]
+    scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
+    # Get the index of the document at the threshold.
+    center = min(
+        range(len(scores)),
+        key=lambda x: abs(scores[x][1]-threshold)
+    )
+
+    # CSV writer.
+    cols = ['id', 'title', 'text']
+    writer = csv.DictWriter(out_file, cols)
+    writer.writeheader()
+
+    for path, score in scores[center-r:center+r]:
+
+        row = (
+            Document_Text
+            .select(Document_Text.text, Document.path)
+            .join(Document)
+            .where(Document.path==path)
+            .naive()
+            .first()
+        )
+
+        writer.writerow({
+            'id': row.path,
+            'title': row.path,
+            'text': row.text
+        })
