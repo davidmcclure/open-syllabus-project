0cd6de140b89313150d695a1c4f5a83178fb7117
David McClure <davidwilliammcclure@gmail.com>
2015-05-21 14:49:55 -0700

    Scrubs out the old rank-caching routines on HLOM_Record.

--- osp/dev.py
+++ osp/dev.py
@@ -1,55 +1,70 @@
 
 
 from osp.citations.hlom.ranking import Ranking
+from osp.citations.hlom.models.record_cited import HLOM_Record_Cited
+from playhouse.postgres_ext import ServerSide
 
 
-r = Ranking()
+#r = Ranking()
 
 
-def print_rank(query):
-    for t in query:
-        print(
-            t['rank'],
-            round(t['score'], 2),
-            t['record'].count,
-            t['record'].marc.title(),
-            t['record'].marc.author()
-        )
+#def print_rank(query):
+    #for t in query:
+        #print(
+            #t['rank'],
+            #round(t['score'], 2),
+            #t['record'].count,
+            #t['record'].marc.title(),
+            #t['record'].marc.author()
+        #)
 
 
-def all_ranks(page_num=1, page_len=100):
+#def all_ranks(page_num=1, page_len=100):
+
+    #"""
+    #Get unfiltered rankings.
+    #"""
+
+    #r.reset()
+    #print_rank(r.rank(page_num, page_len))
+
+
+#def institution_ranks(iid, page_num=1, page_len=100):
+
+    #"""
+    #Get text rankings for an institution.
+
+    #Args:
+        #iid (int): The institution id.
+    #"""
+
+    #r.reset()
+    #r.filter_institution(iid)
+    #print_rank(r.rank(page_num, page_len))
+
+
+#def state_ranks(state, page_num=1, page_len=100):
+
+    #"""
+    #Get text rankings for a state.
+
+    #Args:
+        #state (str): The state abbreviation.
+    #"""
+
+    #r.reset()
+    #r.filter_state(state)
+    #print_rank(r.rank(page_num, page_len))
+
+
+def queries():
 
     """
-    Get unfiltered rankings.
+    Print Elasticsearch queries.
     """
 
-    r.reset()
-    print_rank(r.rank(page_num, page_len))
-
-
-def institution_ranks(iid, page_num=1, page_len=100):
-
-    """
-    Get text rankings for an institution.
-
-    Args:
-        iid (int): The institution id.
-    """
-
-    r.reset()
-    r.filter_institution(iid)
-    print_rank(r.rank(page_num, page_len))
-
-
-def state_ranks(state, page_num=1, page_len=100):
-
-    """
-    Get text rankings for a state.
-
-    Args:
-        state (str): The state abbreviation.
-    """
-
-    r.reset()
-    r.filter_state(state)
-    print_rank(r.rank(page_num, page_len))
+    for r in ServerSide(HLOM_Record_Cited.select()):
+        q = r.query
+        if not q:
+            print(r.marc.title())
+            print(r.marc.author())
