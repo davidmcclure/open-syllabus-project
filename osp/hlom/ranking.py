4680a4b1c1d4141129a902d855561840c56c0d6b
David McClure <davidwilliammcclure@gmail.com>
2015-05-20 17:47:32 -0700

    Revert "Refactors the Ranking class to inherit directly from peewee.SelectQuery."
    This reverts commit dc890bf83af2a64cb5c2a8c1ba1b9efbd9e797be.

--- osp/hlom/ranking.py
+++ osp/hlom/ranking.py
@@ -4,10 +4,10 @@ from osp.citations.hlom.models.citation import HLOM_Citation
 from osp.citations.hlom.models.record_cited import HLOM_Record_Cited
 from osp.locations.models.doc_inst import Document_Institution
 from osp.institutions.models.institution import Institution
-from peewee import fn, SelectQuery
+from peewee import fn
 
 
-class Ranking(SelectQuery):
+class Ranking:
 
 
     def __init__(self):
@@ -16,20 +16,22 @@ class Ranking(SelectQuery):
         Initialize the un-filtered query.
         """
 
-        super().__init__(self, HLOM_Record_Cited)
-
-        # Select record, count.
         count = fn.Count(HLOM_Citation.id)
-        self.select(HLOM_Record_Cited, count)
 
-        # Join citations.
-        self.join(HLOM_Citation, on=(
-            HLOM_Record_Cited.id==HLOM_Citation.record
-        ))
+        self.query = (
 
-        # Order by count.
-        self.group_by(HLOM_Record_Cited.id)
-        self.order_by(count.desc())
+            HLOM_Record_Cited
+            .select(HLOM_Record_Cited, count)
+
+            # Join citations.
+            .join(HLOM_Citation, on=(
+                HLOM_Record_Cited.id==HLOM_Citation.record
+            ))
+
+            .group_by(HLOM_Record_Cited.id)
+            .order_by(count.desc())
+
+        )
 
 
     def filter_institution(self, iid):
