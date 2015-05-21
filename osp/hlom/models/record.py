3ed84a1ad0bfdc0a1532e6e88d87d2a48056f414
David McClure <davidwilliammcclure@gmail.com>
2015-05-21 15:02:41 -0700

    Gets rid of Elasticsearch mixin and methods in HLOM_Record, since it's all PG based now.

--- osp/hlom/models/record.py
+++ osp/hlom/models/record.py
@@ -8,18 +8,17 @@ import hashlib
 from osp.common.config import config
 from osp.common.utils import query_bar
 from osp.common.models.base import BaseModel
-from osp.common.mixins.elasticsearch import Elasticsearch
 from osp.citations.hlom.utils import prettify_field
 from osp.citations.hlom.dataset import Dataset
 from osp.citations.hlom.utils import sanitize_query
-from osp.citations.hlom.marc import MARC
+from pymarc import Record
 from scipy.stats import rankdata
 from clint.textui.progress import bar
 from playhouse.postgres_ext import *
 from peewee import *
 
 
-class HLOM_Record(BaseModel, Elasticsearch):
+class HLOM_Record(BaseModel):
 
 
     control_number = CharField(unique=True, null=False)
@@ -29,77 +28,6 @@ class HLOM_Record(BaseModel, Elasticsearch):
 
     class Meta:
         database = config.get_table_db('hlom_record')
-
-
-    es_index = 'hlom'
-    es_doc_type = 'record'
-
-
-    es_mapping = {
-        '_id': {
-            'index': 'not_analyzed',
-            'store': True
-        },
-        'properties': {
-            'author': {
-                'type': 'string'
-            },
-            'title': {
-                'type': 'string'
-            },
-            'publisher': {
-                'type': 'string'
-            },
-            'pubyear': {
-                'type': 'string'
-            },
-            'count': {
-                'type': 'integer'
-            },
-            'rank': {
-                'type': 'integer'
-            },
-            'score': {
-                'type': 'float'
-            },
-        }
-    }
-
-
-    @classmethod
-    def es_stream_docs(cls):
-
-        """
-        Just index cited rows.
-
-        Yields:
-            dict: The next document.
-        """
-
-        for row in cls.select_cited():
-            yield row.es_doc
-
-
-    @property
-    def es_doc(self):
-
-        """
-        Construct a document for Elasticsearch.
-
-        Returns:
-            dict: The document fields.
-        """
-
-        return {
-            '_id':              self.control_number,
-            'author':           prettify_field(self.marc.author()),
-            'title':            prettify_field(self.marc.title()),
-            'publisher':        prettify_field(self.marc.publisher()),
-            'pubyear':          prettify_field(self.marc.pubyear()),
-            'count':            self.metadata['citation_count'],
-            'rank':             self.metadata['rank'],
-            'score':            self.metadata['score'],
-        }
 
 
     @classmethod
@@ -146,7 +74,7 @@ class HLOM_Record(BaseModel, Elasticsearch):
             pymarc.Record
         """
 
-        return MARC(
+        return Record(
             data=bytes(self.record),
             ascii_handling='ignore',
             utf8_handling='ignore'
@@ -189,10 +117,10 @@ class HLOM_Record(BaseModel, Elasticsearch):
         Build an Elasticsearch query string.
 
         Returns:
-            str: "[title] [author]"
+            str|None: "[title] [author]", or None if invalid.
         """
 
-        return sanitize_query(' '.join([
-            self.marc.title(),
-            self.marc.author()
-        ]))
+        t = self.marc.title()
+        a = self.marc.author()
+
+        return sanitize_query(' '.join([t,a]))
