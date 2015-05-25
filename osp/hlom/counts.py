306288753bc4ec3982a581a8aaa6f6a79b21f199
David McClure <davidwilliammcclure@gmail.com>
2015-05-25 08:26:24 -0700

    Adds a Counts class, which wraps around the counts.csv file.

---
+++ osp/hlom/counts.py
@@ -0,0 +1,56 @@
+
+
+from osp.common.utils import read_csv
+from nltk.stem import PorterStemmer
+
+
+class Counts:
+
+
+    def __init__(self):
+
+        """
+        Read the csv, index counts / ranks.
+        """
+
+        self.stem = PorterStemmer().stem
+
+        self.reader = read_csv(
+            'osp.citations.hlom',
+            'data/counts.csv'
+        )
+
+        self.counts = {}
+        self.ranks  = {}
+
+        for i, row in enumerate(self.reader):
+            self.counts[row['term']] = row['count']
+            self.ranks[row['term']] = i
+
+
+    def count(self, term):
+
+        """
+        Get a term's frequency count.
+
+        Args:
+            term (str): An unstemmed term.
+
+        Returns: int
+        """
+
+        return self.counts[self.stem(term)]
+
+
+    def rank(self, term):
+
+        """
+        Get a term's frequency rank.
+
+        Args:
+            term (str): An unstemmed term.
+
+        Returns: int
+        """
+
+        return self.ranks[self.stem(term)]
