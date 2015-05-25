e2d834cb290d3c867ed6a7f15c19e4a3d1dff0d6
David McClure <davidwilliammcclure@gmail.com>
2015-05-25 10:54:11 -0700

    Breaks the HLOM_Record#hash tests into separate sections.

--- osp/test/citations/hlom/models/record/test_hash.py
+++ osp/test/citations/hlom/models/record/test_hash.py
@@ -3,30 +3,53 @@
 from osp.citations.hlom.models.record import HLOM_Record
 
 
-def test_hash(models, add_hlom):
+def test_ignore_capitalization(add_hlom):
 
     hashes = set()
 
-    # Ignore capitalization.
     hashes.add(add_hlom('Anna Karenina', 'Leo Tolstoy').hash)
     hashes.add(add_hlom('anna karenina', 'leo tolstoy').hash)
     hashes.add(add_hlom('ANNA KARENINA', 'LEO TOLSTOY').hash)
 
-    # Ignore whitespace.
+    assert len(hashes) == 1
+
+
+def test_ignore_whitespace(add_hlom):
+
+    hashes = set()
+
     hashes.add(add_hlom('Anna  Karenina', 'Leo  Tolstoy').hash)
     hashes.add(add_hlom(' Anna Karenina ', ' Leo Tolstoy ').hash)
 
-    # Ignore punctuation.
+    assert len(hashes) == 1
+
+
+def test_ignore_punctuation(add_hlom):
+
+    hashes = set()
+
     hashes.add(add_hlom('Anna Karenina /', 'Leo Tolstoy /').hash)
     hashes.add(add_hlom('Anna Karenina.', 'Leo Tolstoy.').hash)
     hashes.add(add_hlom('"Anna Karenina,"', 'Leo Tolstoy').hash)
 
-    # Ignore articles.
-    hashes.add(add_hlom('The Anna Karenina', 'Leo Tolstoy /').hash)
-    hashes.add(add_hlom('A Anna Karenina', 'Leo Tolstoy.').hash)
-    hashes.add(add_hlom('An Anna Karenina"', 'Leo Tolstoy').hash)
+    assert len(hashes) == 1
 
-    # Ignore author order.
+
+def test_ignore_articles(add_hlom):
+
+    hashes = set()
+
+    hashes.add(add_hlom('The Republic', 'Plato').hash)
+    hashes.add(add_hlom('A Republic', 'Plato').hash)
+    hashes.add(add_hlom('An Republic"', 'Plato').hash)
+
+    assert len(hashes) == 1
+
+
+def test_ignore_author_order(add_hlom):
+
+    hashes = set()
+
     hashes.add(add_hlom('Anna Karenina', 'Leo Tolstoy').hash)
     hashes.add(add_hlom('Anna Karenina', 'Tolstoy, Leo').hash)
 
