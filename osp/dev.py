bf0bff322f14ee4270675d0be1d220fb972a66ca
David McClure <davidwilliammcclure@gmail.com>
2015-05-26 20:26:15 -0700

    Updates the dev module to match changes to the Ranking class.

--- osp/dev.py
+++ osp/dev.py
@@ -5,15 +5,11 @@ from osp.citations.hlom.models.record_cited import HLOM_Record_Cited
 from playhouse.postgres_ext import ServerSide
 
 
-r = Ranking()
-
-
 def print_rank(query):
     for t in query:
         print(
             t['rank'],
             t['record'].count,
-            round(t['score'], 2),
             t['record'].marc.title(),
             t['record'].marc.author()
         )
@@ -25,7 +21,7 @@ def all_ranks(page_num=1, page_len=100):
     Get unfiltered rankings.
     """
 
-    r.reset()
+    r = Ranking()
     print_rank(r.rank(page_num, page_len))
 
 
@@ -38,7 +34,7 @@ def institution_ranks(iid, page_num=1, page_len=100):
         iid (int): The institution id.
     """
 
-    r.reset()
+    r = Ranking()
     r.filter_institution(iid)
     print_rank(r.rank(page_num, page_len))
 
@@ -52,18 +48,6 @@ def state_ranks(state, page_num=1, page_len=100):
         state (str): The state abbreviation.
     """
 
-    r.reset()
+    r = Ranking()
     r.filter_state(state)
     print_rank(r.rank(page_num, page_len))
-
-
-def queries():
-
-    """
-    Print Elasticsearch queries.
-    """
-
-    for r in ServerSide(HLOM_Record_Cited.select()):
-        q = r.query
-        if not q:
-            print(r.marc.title(), r.marc.author())
