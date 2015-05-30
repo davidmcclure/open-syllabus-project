5c330fbc5fa1900c16ea7f8f42990a41e9ea29cc
David McClure <davidwilliammcclure@gmail.com>
2015-05-29 19:26:14 -0700

    Tests Document_Institution.institution_counts().

--- osp/locations/models/doc_inst.py
+++ osp/locations/models/doc_inst.py
@@ -9,8 +9,37 @@ from peewee import *
 
 class Document_Institution(BaseModel):
 
-    document = ForeignKeyField(Document)
+
+    document = ForeignKeyField(Document, unique=True)
     institution = ForeignKeyField(Institution)
+
 
     class Meta:
         database = config.get_table_db('document_institution')
+
+
+    @classmethod
+    def institution_counts(cls):
+
+        """
+        Map institution ids to syllabus counts.
+
+        Returns:
+            dict: id -> count.
+        """
+
+        count = fn.COUNT(cls.document)
+
+        query = (
+            cls.select(cls.institution, count)
+            .distinct([cls.institution])
+            .group_by(cls.institution)
+        )
+
+        print(query.sql())
+
+        counts = {}
+        for row in query:
+            counts[row.count] = row._data['institution']
+
+        return counts
---
+++ osp/test/locations/models/doc_inst/test_institution_counts.py
@@ -0,0 +1,42 @@
+
+
+from osp.institutions.models.institution import Institution
+from osp.locations.models.doc_inst import Document_Institution
+from osp.corpus.models.document import Document
+
+
+def test_institution_counts(models):
+
+    """
+    Document_Institution.institution_counts() should provide syllabus counts
+    for each institution id.
+    """
+
+    i1 = Institution.create()
+    i2 = Institution.create()
+    i3 = Institution.create()
+
+    d1 = Document.create(path='d1')
+    d2 = Document.create(path='d2')
+    d3 = Document.create(path='d3')
+    d4 = Document.create(path='d4')
+    d5 = Document.create(path='d5')
+    d6 = Document.create(path='d6')
+
+    # 1 document for institution 1.
+    Document_Institution.create(institution=i1, document=d1)
+
+    # 2 documents for institution 2.
+    Document_Institution.create(institution=i2, document=d2)
+    Document_Institution.create(institution=i2, document=d3)
+
+    # 3 documents for institution 3.
+    Document_Institution.create(institution=i3, document=d4)
+    Document_Institution.create(institution=i3, document=d5)
+    Document_Institution.create(institution=i3, document=d6)
+
+    assert Document_Institution.institution_counts() == {
+        d1.id: 1,
+        d2.id: 2,
+        d3.id: 3
+    }
