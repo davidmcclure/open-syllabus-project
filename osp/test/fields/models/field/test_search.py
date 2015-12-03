84bfa6f112426c0aa6bc4fd3d3dfd0f9a753af43
David McClure <davidwilliammcclure@gmail.com>
2015-12-03 14:10:40 -0800

    In the field search tests, use pytest.mark.parametrize to structure cases.

--- osp/test/fields/models/field/test_search.py
+++ osp/test/fields/models/field/test_search.py
@@ -5,25 +5,41 @@ import pytest
 from osp.fields.models.field import Field
 
 
-def test_match_name(models):
+@pytest.mark.parametrize('text', [
+    'Field 12',
+    'Field 123',
+    'Field 1234',
+])
+def test_match_name(text, models):
 
     """
     Field#search() should match a field name code in the passed text.
     """
 
     field = Field.create(secondary_name='Field')
+    assert field.search(text) is not None
 
-    assert field.search('Field 101') is not None
 
+@pytest.mark.parametrize('text', [
 
-def test_match_abbreviations(models):
+    'AB 12',
+    'CD 12',
+    'EF 12',
+
+    'AB 123',
+    'CD 123',
+    'EF 123',
+
+    'AB 1234',
+    'CD 1234',
+    'EF 1234',
+
+])
+def test_match_abbreviations(text, models):
 
     """
     Should match abbreviated codes.
     """
 
     field = Field.create(abbreviations=['AB', 'CD', 'EF'])
-
-    assert field.search('AB 101') is not None
-    assert field.search('CD 101') is not None
-    assert field.search('EF 101') is not None
+    assert field.search(text) is not None
