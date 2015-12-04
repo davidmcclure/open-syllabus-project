dcba4ab8dc6be1600dbc1cc09fc120907e350187
David McClure <davidwilliammcclure@gmail.com>
2015-12-03 19:09:02 -0800

    Use parametrization on the parse_abbrs() tests.

--- osp/test/fields/utils/test_parse_abbrs.py
+++ osp/test/fields/utils/test_parse_abbrs.py
@@ -1,20 +1,28 @@
 
 
+import pytest
+
 from osp.fields.utils import parse_abbrs
 
 
-def test_split_on_comma():
-    assert parse_abbrs('AB, CD, EF') == ['AB', 'CD', 'EF']
+@pytest.mark.parametrize('raw,parsed', [
 
-def test_strip_whitespace():
-    assert parse_abbrs('  AB, CD, EF  ') == ['AB', 'CD', 'EF']
-    assert parse_abbrs('\nAB, CD, EF\n') == ['AB', 'CD', 'EF']
+    # Split on comma:
+    ('AB, CD, EF', ['AB', 'CD', 'EF']),
 
-def test_keep_interior_spaces():
-    assert parse_abbrs('AB CD, EF GH') == ['AB CD', 'EF GH']
+    # Strip whitespace:
+    ('  AB, CD, EF  ', ['AB', 'CD', 'EF']),
+    ('\nAB, CD, EF\n', ['AB', 'CD', 'EF']),
 
-def test_ignore_trailing_comma():
-    assert parse_abbrs('AB, CD, EF,') == ['AB', 'CD', 'EF']
+    # Keep interior space:
+    ('AB CD, EF GH', ['AB CD', 'EF GH']),
 
-def test_parse_empty_string_to_empty_array():
-    assert parse_abbrs('') == []
+    # Ignore trailing comma:
+    ('AB, CD, EF,', ['AB', 'CD', 'EF']),
+
+    # Empty string -> []:
+    ('', []),
+
+])
+def test_parse_abbrs(raw, parsed):
+    assert parse_abbrs(raw) == parsed
