60785faa291fed4fbb6b426b0df0aedcf011521b
David McClure <davidwilliammcclure@gmail.com>
2015-12-03 16:36:45 -0800

    Update the fields queue_queries commend.

--- bin/commands/fields.py
+++ bin/commands/fields.py
@@ -2,9 +2,11 @@
 
 import click
 
+from osp.corpus.models.text import Document_Text
 from osp.fields.models.field import Field
 from osp.fields.models.field_document import Field_Document
-from osp.fields.jobs.query import query
+from osp.fields.jobs.ext_fields import ext_fields
+
 from peewee import create_model_tables
 
 
@@ -43,28 +45,5 @@ def queue_queries():
     Queue query tasks in the worker.
     """
 
-    for field in Field.select():
-        config.rq.enqueue(query, field.id)
-
-
-@cli.command()
-@click.argument('code')
-def query(code):
-
-    """
-    Debug a field code query.
-    """
-
-    results = config.es.search('osp', 'document', timeout=30, body={
-        'fields': ['doc_id'],
-        'size': 100000,
-        'filter': {
-            'query': {
-                'regexp': {
-                    'body': '{:s}\s+[0-9]{{2,4}}'.format(code)
-                }
-            }
-        }
-    })
-
-    print(results)
+    for text in Document_Text.select():
+        config.rq.enqueue(ext_fields, text.document_id)
