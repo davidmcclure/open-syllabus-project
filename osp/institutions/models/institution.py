b961d801806480a36384c675ba2098c8f49faf0a
David McClure <davidwilliammcclure@gmail.com>
2015-12-05 11:42:05 -0800

    Insert US institutions.

--- osp/institutions/models/institution.py
+++ osp/institutions/models/institution.py
@@ -1,10 +1,13 @@
 
 
+import pkgutil
+
 from osp.common.config import config
 from osp.common.utils import read_csv, parse_domain
 from osp.common.models.base import BaseModel
 
-from peewee import CharField
+from peewee import CharField, IntegrityError
+from bs4 import BeautifulSoup
 
 
 class Institution(BaseModel):
@@ -25,4 +28,21 @@ class Institution(BaseModel):
         Write institution rows into the database.
         """
 
-        pass
+        html = pkgutil.get_data(
+            'osp.institutions',
+            'data/us-inst.html'
+        )
+
+        root = BeautifulSoup(html.decode('utf8'), 'html.parser')
+
+        for link in root.select('li a'):
+
+            # Clean the name.
+            name = link.get_text().strip()
+
+            # Normalize the domain.
+            domain = parse_domain(link.attrs['href'])
+
+            # Write the row.
+            try: cls.create(name=name, domain=domain)
+            except IntegrityError: pass
