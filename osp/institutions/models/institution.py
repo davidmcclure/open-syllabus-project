2e638330c8cb9f583934eb81e26e4d750a53436c
David McClure <davidwilliammcclure@gmail.com>
2015-12-04 16:38:35 -0800

    Get rid of the institution geocoding job, which we won't be using for a while.

--- osp/institutions/models/institution.py
+++ osp/institutions/models/institution.py
@@ -35,27 +35,3 @@ class Institution(BaseModel):
 
         with cls._meta.database.transaction():
             cls.insert_many(rows).execute()
-
-
-    @property
-    def geocoding_query(self):
-
-        """
-        Build a geolocation query.
-        """
-
-        # Campus address:
-        c_street = self.metadata['Campus_Address']
-        c_city   = self.metadata['Campus_City']
-        c_state  = self.metadata['Campus_State']
-
-        if c_street and c_city and c_state:
-            return ','.join([c_street, c_city, c_state, 'USA'])
-
-        # Or, institution address:
-        i_street = self.metadata['Institution_Address']
-        i_city   = self.metadata['Institution_City']
-        i_state  = self.metadata['Institution_State']
-
-        if i_street and i_city and i_state:
-            return ','.join([i_street, i_city, i_state, 'USA'])
