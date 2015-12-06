de95ea65a8c4427f522de012c2cebbac45b1df76
David McClure <davidwilliammcclure@gmail.com>
2015-12-05 18:07:24 -0800

    Finish failing test for /queue.

--- osp/test/api/server/test_queue.py
+++ osp/test/api/server/test_queue.py
@@ -1,6 +1,8 @@
 
 
 from osp.corpus.models import Document
+from osp.corpus.jobs import ext_text
+from osp.common.utils import partitions
 
 
 def test_queue(models, api_client, queue):
@@ -20,10 +22,19 @@ def test_queue(models, api_client, queue):
         'count':    10,
     })
 
-    # Should queue the meta-job.
+    # Should queue meta-job.
     assert queue.count == 1
 
-    # run meta-job
-    # should queue jobs
-    # check func/args
-    # should return id range
+    # Get the id range.
+    (id1, id2) = partitions(1, Document.max_id(), 10)[5]
+
+    # Run the queue-job.
+    meta = queue.dequeue()
+    meta.perform()
+
+    # Should spool work jobs.
+    assert queue.count == id2-id1+1
+
+    for i, doc_id in enumerate(range(id1, i2+1)):
+        assert queue.jobs[i].func == ext_text
+        assert queue.jobs[i].args == (doc_id,)
