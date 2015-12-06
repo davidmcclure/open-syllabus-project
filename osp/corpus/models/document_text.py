0abdda73fdc0621a3e37b7739f9b2e784b556d94
David McClure <davidwilliammcclure@gmail.com>
2015-12-05 18:30:21 -0800

    Fix circular import.

--- osp/corpus/models/document_text.py
+++ osp/corpus/models/document_text.py
@@ -3,12 +3,10 @@
 import re
 
 from osp.common.config import config
-from osp.common.utils import query_bar, tokenize
+from osp.common.utils import query_bar
 from osp.common.models.base import BaseModel
 from osp.common.mixins.elasticsearch import Elasticsearch
 from osp.corpus.models import Document
-from osp.hlom.models import HLOM_Citation
-from collections import Counter
 from peewee import *
 
 
@@ -44,33 +42,16 @@ class Document_Text(BaseModel, Elasticsearch):
 
 
     @classmethod
-    def es_stream_docs(cls, just_cited=False):
+    def es_stream_docs(cls):
 
         """
         Index all texts.
-
-        Args:
-            just_cited (bool): If true, just index texts with citations.
 
         Yields:
             dict: The next document.
         """
 
-        # By default, index everything.
-        if not just_cited:
-            query = cls.select()
-
-        # Omit texts without citations.
-        else:
-            query = (
-                cls.select()
-                .group_by(cls.id)
-                .join(HLOM_Citation, on=(
-                    cls.document==HLOM_Citation.document
-                ))
-            )
-
-        for row in query_bar(query):
+        for row in query_bar(cls.select()):
             yield row.es_doc
 
 
@@ -89,43 +70,3 @@ class Document_Text(BaseModel, Elasticsearch):
             'doc_id':   self.document.id,
             'body':     self.text
         }
-
-
-    @classmethod
-    def term_counts(cls, limit=None):
-
-        """
-        Get frequency counts for all unique word types.
-
-        Returns:
-            dict: type -> count
-        """
-
-        texts = cls.select().limit(limit)
-        counts = Counter()
-
-        for row in query_bar(texts):
-            row.tokenize()
-            for term, offsets in row.terms.items():
-                counts[term] += len(offsets)
-
-        return counts
-
-
-    def tokenize(self):
-
-        """
-        Tokenize the text.
-        """
-
-        self.tokens = []
-        self.terms  = {}
-
-        for token in tokenize(self.text):
-
-            # Token:
-            self.tokens.append(token)
-
-            # Word type:
-            offsets = self.terms.setdefault(token['stemmed'], [])
-            offsets.append(token['offset'])
