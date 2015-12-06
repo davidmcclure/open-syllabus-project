3ffb1151c183c8af7e232dd57ce18e990a994512
David McClure <davidwilliammcclure@gmail.com>
2015-12-05 18:27:18 -0800

    Stubbing in an implementation for /queue.

--- osp/api/server.py
+++ osp/api/server.py
@@ -3,10 +3,13 @@
 import os
 import importlib
 
+from osp.common.utils import partitions
+from osp.hlom.api import hlom
+from osp.corpus.api import corpus
+
 from flask import Flask
 from rq_dashboard import RQDashboard
-from osp.corpus.api import corpus
-from osp.hlom.api import hlom
+from pydoc import locate
 
 
 # RQ dashboard:
@@ -26,7 +29,32 @@ def ping():
 
 @app.route('/queue', methods=['POST'])
 def queue():
-    pass # TODO
+
+    """
+    Queue a work order.
+    """
+
+    # Get model / job callables.
+    model = locate(request.form['model'])
+    job = locate(request.form['job'])
+
+    # Get worker index and count.
+    count = int(request.form['count'])
+    index = int(request.form['index'])
+
+    # Get id range.
+    (id1, id2) = partitions(1, model.max_id(), count)[index]
+
+    # Meta-job to spool the order.
+    def _meta(id1, id2):
+        for idx in range(id1, id2+1):
+            config.rq.enqueue(job, idx)
+
+    # Queue the meta-job.
+    job = config.rq.enqueue(_meta, id1, id2, timeout=3600)
+
+    code = 200 if job.is_queued else 500
+    return ('', code)
 
 
 if __name__ == '__main__':
