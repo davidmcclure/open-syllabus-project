4eb1178a0fdc11fe82cf7c802fac5802a436b672
David McClure <davidwilliammcclure@gmail.com>
2015-12-08 17:20:45 -0800

    Get rid of unused imports in Field.

--- osp/fields/models/field.py
+++ osp/fields/models/field.py
@@ -2,8 +2,6 @@
 
 from osp.common.config import config
 from osp.common.models.base import BaseModel
-from osp.common.utils import read_csv
-from osp.fields.utils import clean_field_name
 
 from peewee import CharField
 
