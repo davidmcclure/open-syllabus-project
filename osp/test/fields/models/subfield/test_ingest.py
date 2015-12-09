f802191b4bb01dea9ec9d1d357d95495970de773
David McClure <davidwilliammcclure@gmail.com>
2015-12-08 18:04:25 -0800

    Basic test for Subfield.ingest().

---
+++ osp/test/fields/models/subfield/test_ingest.py
@@ -0,0 +1,53 @@
+
+
+from osp.fields.models import Field
+from osp.fields.models import Subfield
+
+
+def test_insert_rows(models):
+
+    """
+    Subfield.ingest() should load field and subfield rows.
+    """
+
+    Subfield.ingest(
+        'osp.test.fields.models.subfield',
+        'fixtures/ingest/insert_rows.csv',
+    )
+
+    assert Field.select().count() == 3
+    assert Subfield.select().count() == 9
+
+    f1 = Field.get(Field.name=='Field1')
+    f2 = Field.get(Field.name=='Field2')
+    f3 = Field.get(Field.name=='Field3')
+
+    sf1 = Subfield.get(Subfield.name=='Subfield1')
+    sf2 = Subfield.get(Subfield.name=='Subfield2')
+    sf3 = Subfield.get(Subfield.name=='Subfield3')
+    sf4 = Subfield.get(Subfield.name=='Subfield4')
+    sf5 = Subfield.get(Subfield.name=='Subfield5')
+    sf6 = Subfield.get(Subfield.name=='Subfield6')
+    sf7 = Subfield.get(Subfield.name=='Subfield7')
+    sf8 = Subfield.get(Subfield.name=='Subfield8')
+    sf9 = Subfield.get(Subfield.name=='Subfield9')
+
+    assert sf1.field == f1
+    assert sf2.field == f1
+    assert sf3.field == f1
+
+    assert sf4.field == f2
+    assert sf5.field == f2
+    assert sf6.field == f2
+
+    assert sf7.field == f3
+    assert sf8.field == f3
+    assert sf9.field == f3
+
+
+def test_clean_field_names():
+    pass
+
+
+def test_filter_abbrs():
+    pass
