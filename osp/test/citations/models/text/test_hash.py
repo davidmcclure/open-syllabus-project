6d9ec184be8a0a4f25181834d9953047e4b57c56
David McClure <davidwilliammcclure@gmail.com>
2015-12-09 15:56:21 -0800

    Don't duplicate tokenize_query() cases in the Text#hash() tests.

--- osp/test/citations/models/text/test_hash.py
+++ osp/test/citations/models/text/test_hash.py
@@ -5,17 +5,10 @@ import pytest
 
 @pytest.mark.parametrize('pairs', [
 
-    # Coalesce formattings.
+    # Coalesce formatting.
     [
         ('Anna Karenina', 'Leo Tolstoy'),
-        ('anna karenina', 'leo tolstoy'),
         ('ANNA KARENINA', 'LEO TOLSTOY'),
-        ('Anna  Karenina', 'Leo  Tolstoy'),
-        (' Anna Karenina ', ' Leo Tolstoy '),
-        ('Anna Karenina /', 'Leo Tolstoy /'),
-        ('Anna Karenina.', 'Leo Tolstoy.'),
-        ('"Anna Karenina,"', 'Leo Tolstoy'),
-        ('Anna Karenina 2', 'Leo Tolstoy, 1828-1910'),
     ],
 
     # Ignore name order.
