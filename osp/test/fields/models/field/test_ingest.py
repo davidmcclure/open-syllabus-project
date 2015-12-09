5c2e55275c85436817141a65359e52e0db4a19f4
David McClure <davidwilliammcclure@gmail.com>
2015-12-08 16:48:31 -0800

    Test that field names are cleaned in Field.ingest().

--- osp/test/fields/models/field/test_ingest.py
+++ osp/test/fields/models/field/test_ingest.py
@@ -6,7 +6,7 @@ from osp.fields.models import Field
 def test_ingest(models):
 
     """
-    Field.ingest() should load rows from a CSV.
+    Field.ingest() should load "alpha" rows from a CSV.
     """
 
     Field.ingest(
@@ -15,6 +15,22 @@ def test_ingest(models):
     )
 
     assert Field.select().count() == 3
+
+    assert Field.select().where(Field.name=='Field1')
+    assert Field.select().where(Field.name=='Field2')
+    assert Field.select().where(Field.name=='Field3')
+
+
+def test_clean_name(models):
+
+    """
+    The field name should be cleaned.
+    """
+
+    Field.ingest(
+        'osp.test.fields.models.field',
+        'fixtures/ingest/clean_name.csv',
+    )
 
     assert Field.select().where(Field.name=='Field1')
     assert Field.select().where(Field.name=='Field2')
