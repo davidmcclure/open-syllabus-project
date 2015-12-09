d3a4a0cb6422ce887c52fcc333da81e34cd68352
David McClure <davidwilliammcclure@gmail.com>
2015-12-09 10:08:45 -0800

    In Text, don't *-import Peewee classes.

--- osp/citations/models/text.py
+++ osp/citations/models/text.py
@@ -13,8 +13,9 @@ from osp.citations.hlom_corpus import HLOM_Corpus
 from osp.citations.utils import sanitize_query
 from pymarc import Record
 from clint.textui.progress import bar
-from playhouse.postgres_ext import *
-from peewee import *
+
+from playhouse.postgres_ext import BinaryJSONField
+from peewee import CharField, BlobField
 
 
 class Text(BaseModel):
