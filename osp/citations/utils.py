1019618a061ab8e05ba19b14cdaa271fa922aba9
David McClure <davidwilliammcclure@gmail.com>
2015-12-10 14:35:57 -0800

    Add a tokenize_field() helper, which just tokenizes and remove articles.

--- osp/citations/utils.py
+++ osp/citations/utils.py
@@ -29,3 +29,24 @@ def tokenize_query(title, author):
     tokens = [t for t in tokens if t not in ['a', 'an', 'the']]
 
     return tokens
+
+
+def tokenize_field(value):
+
+    """
+    Extract normalized tokens from a field.
+
+    Args:
+        value (str): The field value.
+
+    Returns:
+        list: The cleaned tokens.
+    """
+
+    # Extract tokens.
+    tokens = re.findall('[a-z]+', value.lower())
+
+    # Remove articles.
+    tokens = [t for t in tokens if t not in ['a', 'an', 'the']]
+
+    return tokens
---
+++ osp/test/citations/utils/test_tokenize_field.py
@@ -0,0 +1,64 @@
+
+
+import pytest
+
+from osp.citations.utils import tokenize_field
+
+
+@pytest.mark.parametrize('values,tokens', [
+
+    # Downcase.
+    (
+        [
+            'Anna Karenina',
+            'anna karenina',
+            'ANNA KARENINA',
+        ],
+        ['anna', 'karenina']
+    ),
+
+    # Ignore whitespace.
+    (
+        [
+            'Leo  Tolstoy',
+            '  Leo Tolstoy  ',
+            '\nLeo Tolstoy\n',
+        ],
+        ['leo', 'tolstoy']
+    ),
+
+    # Ignore punctuation.
+    (
+        [
+            'Anna Karenina /',
+            'Anna Karenina.',
+            '"Anna Karenina."',
+        ],
+        ['anna', 'karenina']
+    ),
+
+    # Ignore numbers.
+    (
+        [
+            'Leo Tolstoy 5',
+            'Leo Tolstoy, 1828-1910',
+
+        ],
+        ['leo', 'tolstoy']
+    ),
+
+    # Ignore articles.
+    (
+        [
+            'The War and Peace',
+            'A War and Peace',
+            'An War and Peace',
+
+        ],
+        ['war', 'and', 'peace']
+    ),
+
+])
+def test_tokenize_field(values, tokens, add_text):
+    for value in values:
+        assert tokenize_field(value) == tokens
