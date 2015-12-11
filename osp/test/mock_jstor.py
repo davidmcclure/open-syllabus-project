8195d5815e9e7b1ed7c7d56190d76b73200fdabd
David McClure <davidwilliammcclure@gmail.com>
2015-12-11 15:44:24 -0800

    Implement a first version of Mock_JSTOR#add_record().

--- osp/test/mock_jstor.py
+++ osp/test/mock_jstor.py
@@ -1,7 +1,79 @@
 
 
+import os
+import pkgutil
+import datetime
+import uuid
+
+from jinja2 import Template
 from osp.test.mock_corpus import Mock_Corpus
 
 
 class Mock_JSTOR(Mock_Corpus):
-    pass
+
+
+    def __init__(self):
+
+        """
+        Compile the XML template.
+        """
+
+        super().__init__()
+
+        tpl = pkgutil.get_data('osp.test', 'templates/jstor.j2')
+        self.template = Template(tpl.decode('utf8'))
+
+
+    def add_record(self,
+
+        journal_slug='journal',
+        article_id=None,
+
+        journal_title='Journal',
+        publisher_name='Publisher',
+        article_title='Article',
+
+        pub_year=1987,
+        pub_month=6,
+        pub_day=25,
+
+        authors=[('David W.', 'McClure'),],
+
+    ):
+
+        """
+        Add an XML record to the corpus.
+
+        Returns:
+            str: The path of the new file.
+        """
+
+        # Generate an article id.
+        if not article_id:
+            article_id = str(uuid.uuid4())
+
+        args = locals().copy()
+        args.pop('self')
+
+        # Template the XML.
+        xml = self.template.render(args)
+
+        # Form the publication date.
+        date = datetime.date(pub_year, pub_month, pub_day)
+
+        rel_path = '/'.join([
+            journal_slug,
+            date.isoformat(),
+            article_id,
+            article_id+'.xml',
+        ])
+
+        path = os.path.join(self.path, rel_path)
+
+        # Ensure the directory.
+        os.makedirs(os.path.dirname(path))
+
+        with open(path, 'w') as fh:
+            print(xml, file=fh)
+
+        return path
