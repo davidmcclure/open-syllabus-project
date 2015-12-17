b711c07ce6aa5f3e02be54f911a3295165a0184a
David McClure <davidwilliammcclure@gmail.com>
2015-12-17 09:49:56 -0800

    Add a get_text() helper.

--- osp/citations/utils.py
+++ osp/citations/utils.py
@@ -54,3 +54,26 @@ def get_min_freq(tokens, lang='en'):
     """
 
     return min([word_frequency(t, lang)*1e6 for t in tokens])
+
+
+def get_text(tree, selector):
+
+    """
+    Extract text from an element. Return None if the element is missing or the
+    value is empty.
+
+    Args:
+        tree (BeautifulSoup): A bs4 tree.
+        selector (str): A CSS selector.
+
+    Returns:
+        str|None
+    """
+
+    tag = tree.select_one(selector)
+
+    if tag:
+        return tag.get_text(strip=True) or None
+
+    else:
+        return None
---
+++ osp/test/citations/utils/test_get_text.py
@@ -0,0 +1,26 @@
+
+
+import pytest
+
+from osp.citations.utils import get_text
+from bs4 import BeautifulSoup
+
+
+@pytest.mark.parametrize('tag,text', [
+
+    ('<tag>Article Title</tag>', 'Article Title'),
+
+    # Strip whitespace.
+    ('<tag>  Article Title  </tag>', 'Article Title'),
+
+    # Empty tag -> None.
+    ('<tag></tag>', None),
+    ('<tag>  </tag>', None),
+
+    # Missing tag -> None.
+    ('', None),
+
+])
+def test_get_text(tag, text):
+    tree = BeautifulSoup(tag, 'lxml')
+    assert get_text(tree, 'tag') == text
