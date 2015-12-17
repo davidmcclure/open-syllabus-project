bcecfbfe0a6f94d592c1e333ba45523479122505
David McClure <davidwilliammcclure@gmail.com>
2015-12-17 14:15:00 -0800

    Add a Text_Index perf class.

--- osp/perf.py
+++ osp/perf.py
@@ -1,10 +1,41 @@
 
 
 import random
+import requests
+import re
 
 from osp.common.mixins.elasticsearch import Elasticsearch
 
 from clint.textui import progress
+
+
+class Faker:
+
+
+    def __init__(self, text_url='http://goo.gl/OJR4J0'):
+
+        """
+        Load and clean a text URL.
+
+        Args:
+            text_url (str)
+        """
+
+        r = requests.get(text_url)
+        self.text = re.sub('\s{2,}', ' ', r.text).strip()
+
+
+    def snippet(self, length):
+
+        """
+        Get a random text snippet.
+
+        Args:
+            length (int)
+        """
+
+        start = random.randrange(0, len(self.text)-length)
+        return self.text[start:start+length]
 
 
 class Citation_Index(Elasticsearch):
@@ -68,4 +99,54 @@ class Citation_Index(Elasticsearch):
                 institution_id  = random.randint(0, 1000),
                 field_id        = random.randint(0, 10),
                 subfield_id     = random.randint(0, 200),
+            )
+
+
+class Text_Index(Elasticsearch):
+
+
+    es_index = 'osp'
+    es_doc_type = 'text'
+
+
+    es_mapping = {
+        '_id': {
+            'path': 'text_id',
+        },
+        'properties': {
+            'text_id': {
+                'type': 'integer'
+            },
+            'title': {
+                'type': 'string'
+            },
+            'author': {
+                'type': 'string'
+            },
+            'publisher': {
+                'type': 'string'
+            },
+        }
+    }
+
+
+    @classmethod
+    def es_stream_docs(cls):
+
+        """
+        Stream Elasticsearch docs.
+
+        Yields:
+            dict: The next document.
+        """
+
+        faker = Faker()
+
+        for i in progress.bar(range(200000)):
+
+            yield dict(
+                text_id     = i,
+                title       = faker.snippet(100),
+                author      = faker.snippet(40),
+                publisher   = faker.snippet(60),
             )
