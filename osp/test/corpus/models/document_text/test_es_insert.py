22af0d28d6370c35b1570ef790b06ca30b0ed873
David McClure <davidwilliammcclure@gmail.com>
2015-12-17 18:17:40 -0800

    Simplify the Document_Text.es_insert() tests.

--- osp/test/corpus/models/document_text/test_es_insert.py
+++ osp/test/corpus/models/document_text/test_es_insert.py
@@ -5,28 +5,21 @@ from osp.corpus.models import Document
 from osp.corpus.models import Document_Text
 
 
-def test_es_insert(models, corpus_index):
+def test_es_insert(corpus_index, add_doc):
 
     """
     Document_Text.es_insert() should index the document body and id.
     """
 
-    # Index 100 documents.
-    for i in range(10):
-        doc = Document.create(path=str(i))
-        Document_Text.create(document=doc, text=str(i))
+    doc = add_doc(content='text')
 
     Document_Text.es_insert()
 
-    # Should insert 10 docs.
-    assert Document_Text.es_count() == 10
+    es_doc = config.es.get(
+        index='osp',
+        doc_type='document',
+        id=doc.id,
+    )
 
-    # For each text row:
-    for t in Document_Text.select():
-
-        # A document should exist.
-        doc = config.es.get('osp', t.document.id)
-
-        # Should index id + text.
-        assert doc['_id'] == str(t.document.id)
-        assert doc['_source']['body'] == t.document.path
+    assert es_doc['_id'] == str(doc.id)
+    assert es_doc['_source']['body'] == 'text'
