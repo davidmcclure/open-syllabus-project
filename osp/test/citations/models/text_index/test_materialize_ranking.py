5f8e252c5060af0b9d24004017bf11b9b4dab5b8
David McClure <davidwilliammcclure@gmail.com>
2015-12-19 12:52:25 -0800

    Failing test for title filtering in materialize_ranking().

--- osp/test/citations/models/text_index/test_materialize_ranking.py
+++ osp/test/citations/models/text_index/test_materialize_ranking.py
@@ -6,7 +6,6 @@ from osp.citations.models import Citation_Index
 from osp.citations.models import Text_Index
 
 
-@pytest.mark.dev
 def test_sort_by_count(add_text, add_citation):
 
     """
@@ -35,3 +34,40 @@ def test_sort_by_count(add_text, add_citation):
     assert texts['hits'][0]['_id'] == str(t3.id)
     assert texts['hits'][1]['_id'] == str(t2.id)
     assert texts['hits'][2]['_id'] == str(t1.id)
+
+
+@pytest.mark.dev
+def test_query_title(add_text, add_citation):
+
+    """
+    If a search query is passed, filter results on title.
+    """
+
+    t1 = add_text(title='David McClure')
+    t2 = add_text(title='Joe Karaganis')
+    t3 = add_text(title='David William')
+    t4 = add_text(title='Dennis Tenen')
+
+    add_citation(t1)
+    add_citation(t1)
+    add_citation(t1)
+    add_citation(t1)
+
+    add_citation(t2)
+    add_citation(t2)
+    add_citation(t2)
+
+    add_citation(t3)
+    add_citation(t3)
+
+    add_citation(t4)
+
+    Citation_Index.es_insert()
+    Text_Index.es_insert()
+
+    ranks = Citation_Index.rank_texts()
+    texts = Text_Index.materialize_ranking(ranks, query='David')
+
+    assert len(texts['hits']) == 2
+    assert texts['hits'][0]['_id'] == str(t1.id)
+    assert texts['hits'][0]['_id'] == str(t3.id)
