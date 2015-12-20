050282a753134889881bac9c6a7c9d443b9dba3f
David McClure <davidwilliammcclure@gmail.com>
2015-12-20 10:50:30 -0800

    Stubbing in the web app and utils.

---
+++ osp/web/app.py
@@ -0,0 +1,17 @@
+
+
+import os
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/ping')
+def ping():
+    return ('web', 200)
+
+
+if __name__ == '__main__':
+    app.run(port=os.getenv('PORT', 5000))
---
+++ osp/web/utils.py
@@ -0,0 +1,32 @@
+
+
+def rank_texts():
+    pass
+
+
+def assigned_with():
+    pass
+
+
+def corpus_facets():
+    pass
+
+
+def subfield_facets():
+    pass
+
+
+def field_facets():
+    pass
+
+
+def institution_facets():
+    pass
+
+
+def state_facets():
+    pass
+
+
+def country_facets():
+    pass
