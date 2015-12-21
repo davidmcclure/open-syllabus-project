bd7e632f1cdfef900e28c493cbc59e43b1dea62a
David McClure <davidwilliammcclure@gmail.com>
2015-12-21 12:49:12 -0800

    Stub in complete tests for rank_texts().

--- osp/test/web/utils/test_rank_texts.py
+++ osp/test/web/utils/test_rank_texts.py
@@ -2,9 +2,38 @@
 
 from osp.web.utils import rank_texts
 from osp.citations.models import Citation_Index
+from osp.citations.models import Text_Index
 
 
-def test_filters(add_text, add_citation):
+def test_unfiltered(add_text, add_citation):
+
+    """
+    When no filters or query is passed, return the overall rankings.
+    """
+
+    t1 = add_text()
+    t2 = add_text()
+    t3 = add_text()
+
+    for i in range(3):
+        add_citation(t1)
+
+    for i in range(2):
+        add_citation(t2)
+
+    for i in range(1):
+        add_citation(t3)
+
+    Citation_Index.es_insert()
+
+    texts = rank_texts()
+
+    assert texts['hits'][0]['_id'] == str(t1.id)
+    assert texts['hits'][1]['_id'] == str(t2.id)
+    assert texts['hits'][2]['_id'] == str(t3.id)
+
+
+def test_metadata_filters(add_text, add_citation):
 
     """
     Citation metadata filters should be applied.
@@ -25,9 +54,42 @@ def test_filters(add_text, add_citation):
 
     Citation_Index.es_insert()
 
-    texts = rank_texts(dict(
+    texts = rank_texts(filters=dict(
         corpus='corpus2'
     ))
 
     assert texts['hits'][0]['_id'] == str(t2.id)
+    assert texts['hits'][1]['_id'] == str(t3.id)
+
+
+def test_search_filter(add_text, add_citation):
+
+    """
+    Free-text search query should be applied.
+    """
+
+    t1 = add_text(title='match one')
+    t2 = add_text(title='two')
+    t3 = add_text(title='match three')
+    t4 = add_text(title='four')
+
+    for i in range(4):
+        add_citation(t1)
+
+    for i in range(3):
+        add_citation(t2)
+
+    for i in range(2):
+        add_citation(t3)
+
+    for i in range(1):
+        add_citation(t4)
+
+    Citation_Index.es_insert()
+    Text_Index.es_insert()
+
+    texts = rank_texts(query='match')
+
+    assert len(texts['hits']) == 2
+    assert texts['hits'][0]['_id'] == str(t1.id)
     assert texts['hits'][1]['_id'] == str(t3.id)
