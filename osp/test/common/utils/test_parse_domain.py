bf9992afbd3819c7bf2a459efcecfaa8e9e902f4
David McClure <davidwilliammcclure@gmail.com>
2015-12-29 15:15:19 -0800

    Harden up the parse_domain() tests.

--- osp/test/common/utils/test_parse_domain.py
+++ osp/test/common/utils/test_parse_domain.py
@@ -7,25 +7,59 @@ from osp.common.utils import parse_domain
 
 @pytest.mark.parametrize('url,domain', [
 
-    ('test.edu', 'test.edu'),
+    # Unchanged
+    (
+        'test.edu',
+        'test.edu',
+    ),
 
-    # Strip protocol:
-    ('http://test.edu', 'test.edu'),
-    ('https://test.edu', 'test.edu'),
+    # Strip protocol
+    (
+        'http://test.edu',
+        'test.edu',
+    ),
+    (
+        'https://test.edu',
+        'test.edu',
+    ),
 
-    # Strip subdomain:
-    ('www.test.edu', 'test.edu'),
-    ('www.sub.test.edu', 'test.edu'),
+    # Strip subdomain
+    (
+        'www.test.edu',
+        'test.edu',
+    ),
+    (
+        'www.sub.test.edu',
+        'test.edu',
+    ),
 
-    # Strip whitespace:
-    ('  http://test.edu  ', 'test.edu'),
+    # Strip suffix
+    (
+        'http://test.edu/syllabus.pdf',
+        'test.edu',
+    ),
 
-    # Downcase:
-    ('WWW.TEST.EDU', 'test.edu'),
+    # Strip whitespace
+    (
+        '  http://test.edu  ',
+        'test.edu',
+    ),
 
-    # Parse archive URLs:
-    ('https://web.archive.org/123/http:/test.edu', 'test.edu'),
-    ('https://web.archive.org/123/https:/test.edu', 'test.edu'),
+    # Downcase
+    (
+        'WWW.TEST.EDU',
+        'test.edu',
+    ),
+
+    # Parse archive URLs
+    (
+        'https://web.archive.org/123/http:/test.edu/syllabus.pdf',
+        'test.edu',
+    ),
+    (
+        'https://web.archive.org/123/https:/test.edu/syllabus.pdf',
+        'test.edu',
+    ),
 
 ])
 def test_parse_domain(url, domain):
