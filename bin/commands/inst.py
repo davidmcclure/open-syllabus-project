6d4d2f768a7a6504482f385721315dbd45d1618b
David McClure <davidwilliammcclure@gmail.com>
2015-12-29 15:27:38 -0800

    Add run_doc_to_inst CLI command.

--- bin/commands/inst.py
+++ bin/commands/inst.py
@@ -2,8 +2,12 @@
 
 import click
 
+from osp.corpus.models import Document
+from osp.common.utils import query_bar
 from osp.institutions.models import Institution
 from osp.institutions.models import Institution_Document
+from osp.institutions.jobs import doc_to_inst
+
 from peewee import create_model_tables
 
 
@@ -34,3 +38,14 @@ def ingest():
 
     Institution.ingest_usa()
     Institution.ingest_world()
+
+
+@cli.command()
+def run_doc_to_inst():
+
+    """
+    Match documents -> institutions.
+    """
+
+    for doc in query_bar(Document.select()):
+        doc_to_inst(doc.id)
