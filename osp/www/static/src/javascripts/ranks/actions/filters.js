e14d1046082a8e2a2ccb3a0d99faac0eae888817
David McClure <davidwilliammcclure@gmail.com>
2015-12-31 11:58:34 -0800

    Adds action creators and reducer listeners for the rest of the filter types.

--- osp/www/static/src/javascripts/ranks/actions/filters.js
+++ osp/www/static/src/javascripts/ranks/actions/filters.js
@@ -3,6 +3,10 @@
 import {
   CHANGE_CORPUS_FILTER,
   CHANGE_FIELD_FILTER,
+  CHANGE_SUBFIELD_FILTER,
+  CHANGE_INSTITUTION_FILTER,
+  CHANGE_STATE_FILTER,
+  CHANGE_COUNTRY_FILTER,
 } from '../constants';
 
 
@@ -27,6 +31,58 @@ export function changeCorpusFilter(values) {
 export function changeFieldFilter(values) {
   return {
     type: CHANGE_FIELD_FILTER,
+    values,
+  };
+}
+
+
+/**
+ * When the subfield filter is changed.
+ *
+ * @param {Mixed} values
+ */
+export function changeSubfieldFilter(values) {
+  return {
+    type: CHANGE_SUBFIELD_FILTER,
+    values,
+  };
+}
+
+
+/**
+ * When the institution filter is changed.
+ *
+ * @param {Mixed} values
+ */
+export function changeInstitutionFilter(values) {
+  return {
+    type: CHANGE_INSTITUTION_FILTER,
+    values,
+  };
+}
+
+
+/**
+ * When the state filter is changed.
+ *
+ * @param {Mixed} values
+ */
+export function changeStateFilter(values) {
+  return {
+    type: CHANGE_STATE_FILTER,
+    values,
+  };
+}
+
+
+/**
+ * When the country filter is changed.
+ *
+ * @param {Mixed} values
+ */
+export function changeCountryFilter(values) {
+  return {
+    type: CHANGE_COUNTRY_FILTER,
     values,
   };
 }
--- osp/www/static/src/javascripts/ranks/reducers/filters.js
+++ osp/www/static/src/javascripts/ranks/reducers/filters.js
@@ -5,12 +5,20 @@ import { createReducer } from '../utils';
 import {
   CHANGE_CORPUS_FILTER,
   CHANGE_FIELD_FILTER,
+  CHANGE_SUBFIELD_FILTER,
+  CHANGE_INSTITUTION_FILTER,
+  CHANGE_STATE_FILTER,
+  CHANGE_COUNTRY_FILTER,
 } from '../constants';
 
 
 const initialState = {
   corpus: null,
   field_id: null,
+  subfield_id: null,
+  institution_id: null,
+  state: null,
+  country: null,
 };
 
 
@@ -22,7 +30,23 @@ const handlers = {
 
   [CHANGE_FIELD_FILTER]: (state, action) => ({
     field_id: action.values
-  })
+  }),
+
+  [CHANGE_SUBFIELD_FILTER]: (state, action) => ({
+    subfield_id: action.values
+  }),
+
+  [CHANGE_INSTITUTION_FILTER]: (state, action) => ({
+    institution_id: action.values
+  }),
+
+  [CHANGE_STATE_FILTER]: (state, action) => ({
+    state: action.values
+  }),
+
+  [CHANGE_COUNTRY_FILTER]: (state, action) => ({
+    country: action.values
+  }),
 
 };
 
