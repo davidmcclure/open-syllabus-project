b5c067ac0074e7150d5d28e68ab06c480f10b792
David McClure <davidwilliammcclure@gmail.com>
2016-01-05 10:14:57 -0800

    Break out separate validation helpers in Validator.

--- osp/citations/validator.py
+++ osp/citations/validator.py
@@ -35,25 +35,69 @@ class Validator:
 
         # Reject duplicates:
 
-        pair = (text.hash, citation.document_id)
-
-        if pair in self.seen:
+        if self.is_duplicate(citation):
             return False
-
-        else:
-            self.seen.add(pair)
 
         # Reject title == author:
 
-        if text.author_tokens == text.title_tokens:
+        elif self.title_same_as_author(text):
             return False
 
-        # Reject blacklisted, single-token titles.
+        # Reject blacklisted titles.
 
-        if (
-            len(text.title_tokens) == 1 and
-            text.title_tokens[0] in self.config['blacklisted_titles']
-        ):
+        elif self.title_blacklisted(text):
             return False
 
         return True
+
+
+    def is_duplicate(self, citation):
+
+        """
+        Is a citation a duplicate?
+
+        Args:
+            citation (osp.models.Citation)
+
+        Returns: bool
+        """
+
+        pair = (citation.text.hash, citation.document_id)
+
+        if pair in self.seen:
+            return True
+
+        else:
+            self.seen.add(pair)
+            return False
+
+
+    def title_same_as_author(self, text):
+
+        """
+        Is a text's title the same as the author?
+
+        Args:
+            citation (osp.models.Text)
+
+        Returns: bool
+        """
+
+        return text.author_tokens == text.title_tokens
+
+
+    def title_blacklisted(self, text):
+
+        """
+        Is a text's title a single blacklisted token?
+
+        Args:
+            citation (osp.models.Text)
+
+        Returns: bool
+        """
+
+        return (
+            len(text.title_tokens) == 1 and
+            text.title_tokens[0] in self.config['blacklisted_titles']
+        )
