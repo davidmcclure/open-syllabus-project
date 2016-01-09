8358789574addba42cee3a426fe684f2ea1f1b6b
David McClure <davidwilliammcclure@gmail.com>
2016-01-09 11:47:15 -0800

    Stub in count indexing in Metrics.

--- osp/www/metrics.py
+++ osp/www/metrics.py
@@ -1,8 +1,10 @@
 
 
 from redis import StrictRedis
+from scipy.stats import rankdata
 
 from osp.common import config
+from osp.citations.models import Citation_Index
 
 
 class Metrics:
@@ -15,3 +17,31 @@ class Metrics:
         """
 
         self.redis = StrictRedis(**config['redis'])
+
+
+    def index_counts(self):
+
+        """
+        Index the raw text counts.
+        """
+
+        counts = Citation_Index.compute_ranking()
+
+        # Text -> total count.
+        self.redis.hmset('osp-counts', counts)
+
+
+    def count(self, text_id):
+
+        """
+        Get the citation count for a text.
+
+        Args:
+            text_id (int)
+
+        Returns: int
+        """
+
+        count = self.redis.hget('osp-counts', text_id)
+
+        return int(count) if count else None
