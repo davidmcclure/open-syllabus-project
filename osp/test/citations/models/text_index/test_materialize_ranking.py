e66ceb7ec0d0a31b5a2fc16ed11205bc11eda3f9
David McClure <davidwilliammcclure@gmail.com>
2016-01-12 19:09:25 -0800

    Test free text search in materialize_ranking().

--- osp/test/citations/models/text_index/test_materialize_ranking.py
+++ osp/test/citations/models/text_index/test_materialize_ranking.py
@@ -9,7 +9,7 @@ from osp.citations.models import Text_Index
 pytestmark = pytest.mark.usefixtures('db', 'es')
 
 
-def test_sort_on_total_count_by_default(add_text, add_citation):
+def test_sort_on_total_counts_by_default(add_text, add_citation):
 
     """
     By default return results sorted on the total citation count.
@@ -69,71 +69,39 @@ def test_sort_on_filtered_counts(add_text, add_citation):
     assert texts['hits'][2]['_id'] == str(t1.id)
 
 
-# def test_sort_by_count(add_text, add_citation):
+@pytest.mark.parametrize('params', [
+    lambda x: dict(title=x),
+    lambda x: dict(authors=[x]),
+    lambda x: dict(publisher=x),
+    lambda x: dict(journal_title=x),
+])
+def test_search_on_total_counts(params, add_text, add_citation):
 
-    # """
-    # Results should be sorted by count descending.
-    # """
+    """
+    If a search query is provided, filter the results on the query.
+    """
 
-    # t1 = add_text()
-    # t2 = add_text()
-    # t3 = add_text()
+    t1 = add_text(**params('match one'))
+    t2 = add_text(**params('two'))
+    t3 = add_text(**params('match three'))
+    t4 = add_text(**params('four'))
 
-    # for i in range(3):
-        # add_citation(t1)
+    for i in range(4):
+        add_citation(t1)
 
-    # for i in range(2):
-        # add_citation(t2)
+    for i in range(3):
+        add_citation(t2)
 
-    # for i in range(1):
-        # add_citation(t3)
+    for i in range(2):
+        add_citation(t3)
 
-    # Citation_Index.es_insert()
-    # Text_Index.es_insert()
+    for i in range(1):
+        add_citation(t4)
 
-    # ranks = Citation_Index.compute_ranking()
-    # texts = Text_Index.materialize_ranking(ranks)
+    Text_Index.es_insert()
 
-    # assert texts['hits'][0]['_id'] == str(t1.id)
-    # assert texts['hits'][1]['_id'] == str(t2.id)
-    # assert texts['hits'][2]['_id'] == str(t3.id)
+    texts = Text_Index.materialize_ranking(query='match')
 
-
-# @pytest.mark.parametrize('params', [
-    # lambda x: dict(title=x),
-    # lambda x: dict(authors=[x]),
-    # lambda x: dict(publisher=x),
-    # lambda x: dict(journal_title=x),
-# ])
-# def test_search(params, add_text, add_citation):
-
-    # """
-    # If a free-text search is passed, query against text metadata.
-    # """
-
-    # t1 = add_text(**params('match one'))
-    # t2 = add_text(**params('two'))
-    # t3 = add_text(**params('match three'))
-    # t4 = add_text(**params('four'))
-
-    # for i in range(4):
-        # add_citation(t1)
-
-    # for i in range(3):
-        # add_citation(t2)
-
-    # for i in range(2):
-        # add_citation(t3)
-
-    # for i in range(1):
-        # add_citation(t4)
-
-    # Citation_Index.es_insert()
-    # Text_Index.es_insert()
-
-    # ranks = Citation_Index.compute_ranking()
-    # texts = Text_Index.materialize_ranking(ranks, query='match')
-
-    # assert len(texts['hits']) == 2
-    # assert texts['hits'][0]['_id'] == str(t1.id)
-    # assert texts['hits'][1]['_id'] == str(t3.id)
+    assert len(texts['hits']) == 2
+    assert texts['hits'][0]['_id'] == str(t1.id)
+    assert texts['hits'][1]['_id'] == str(t3.id)
