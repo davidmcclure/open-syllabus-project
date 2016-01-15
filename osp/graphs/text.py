e520e2dd2ab9d5a4617f83eb9cbc68d867c2d649
David McClure <davidwilliammcclure@gmail.com>
2016-01-15 11:40:51 -0800

    Stub in a Text_Graph class.

---
+++ osp/graphs/text.py
@@ -0,0 +1,33 @@
+
+
+import networkx as nx
+
+
+class Text_Graph:
+
+
+    def __init__(self):
+
+        """
+        Initialize the graph instance.
+        """
+
+        self.graph = nx.Graph()
+
+
+    def add_edges(self):
+
+        """
+        For each syllabus, register citation pairs as edges.
+        """
+
+        pass
+
+
+    def hydrate_nodes(self):
+
+        """
+        Load text metadata onto the nodes.
+        """
+
+        pass
