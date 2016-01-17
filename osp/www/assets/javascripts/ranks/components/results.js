8fcc1c604537d74e5bf6ccd201bf6f83b9d7e82d
David McClure <davidwilliammcclure@gmail.com>
2016-01-17 13:43:05 -0800

    Revert "Get rid of the loadResults() calls in <Results />."
    This reverts commit e1bc1e8dfb757481732941b96ad1f28297ed1bc9.

--- osp/www/assets/javascripts/ranks/components/results.js
+++ osp/www/assets/javascripts/ranks/components/results.js
@@ -2,14 +2,39 @@
 
 import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 import HitCount from './hit-count';
 import ClearFilters from './clear-filters';
 import Search from './search';
 import TextList from './text-list';
 
+import * as actions from '../actions/results';
 
+
+@connect(
+  state => ({
+    filters: state.filters
+  }),
+  actions
+)
 export default class extends Component {
+
+
+  /**
+   * Query for initial results.
+   */
+  componentDidMount() {
+    this.props.loadResults(this.props.filters);
+  }
+
+
+  /**
+   * Query for new results when filters change.
+   */
+  componentDidUpdate() {
+    this.props.loadResults(this.props.filters);
+  }
 
 
   /**
