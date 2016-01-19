042ae518b935ec6a6ff388a5e09ec4cad23d2135
David McClure <davidwilliammcclure@gmail.com>
2016-01-19 09:23:56 -0800

    Sketch in the basic node rendering logic in Gephi_Graph#render().

--- osp/graphs/gephi_graph.py
+++ osp/graphs/gephi_graph.py
@@ -1,12 +1,13 @@
 
 
-import networkx as nx
-import os
 import math
-
-from cached_property import cached_property
+import os
+import pgmagick as pgm
 
 from osp.graphs.graph import Graph
+
+from cached_property import cached_property
+from clint.textui import progress
 
 
 class Gephi_Graph(Graph):
@@ -90,3 +91,50 @@ class Gephi_Graph(Graph):
         """
 
         return math.ceil(self.max_x - self.min_x)
+
+
+    @cached_property
+    def render(self, out_path, size=10000, scale=5, bg_color='#003059'):
+
+        """
+        Render a PNG.
+        """
+
+        image = pgm.Image(
+            pgm.Geometry(size, size),
+            pgm.Color(bg_color),
+        )
+
+        # TODO: font
+
+        nodes = self.graph.nodes_iter(data=True)
+        count = len(self.graph)
+
+        for tid, n in progress.bar(nodes, expected_size=count):
+
+            # Get X/Y, radius.
+            x =  (n['x']*scale) + (size/2)
+            y = -(n['y']*scale) + (size/2)
+            r =  (n['radius']*scale) / 2
+
+            # Index the coordinates.
+            self.graph.node[tid]['pixel_x'] = x
+            self.graph.node[tid]['pixel_y'] = y
+            self.graph.node[tid]['pixel_r'] = r
+
+            # TODO: node color
+
+            # Draw the node.
+            node = pgm.DrawableList()
+            node.append(pgm.DrawableFillColor('gray'))
+            node.append(pgm.DrawableStrokeColor('black'))
+            node.append(pgm.DrawableStrokeWidth(r/15))
+            node.append(pgm.DrawableStrokeOpacity(0.9))
+            node.append(pgm.DrawableCircle(x, y, x+r, y+r))
+            image.draw(node)
+
+            label = ', '.join(n['title'], n['author'])
+
+            # TODO: label
+
+        image.write(os.path.abspath(out_path))
