13f262d7969a6a85ae38d7d9be14bc778ea63249
David McClure <davidwilliammcclure@gmail.com>
2016-01-19 19:34:35 -0800

    Instantiate Leaflet.

--- osp/www/assets/javascripts/graph/components/image.js
+++ osp/www/assets/javascripts/graph/components/image.js
@@ -1,16 +1,38 @@
 
 
 import React, { Component } from 'react';
+import Leaflet from 'leaflet';
+import { findDOMNode } from 'react-dom';
 
 
 export default class extends Component {
 
 
   /**
+   * Spin up the leaflet instance.
+   */
+  componentDidMount() {
+
+    let el = findDOMNode(this.refs.image);
+
+    this.map = Leaflet.map(el, {
+      zoomControl: false,
+    });
+
+    let zoomControl = L.control.zoom({
+      position: 'topright',
+    });
+
+    this.map.addControl(zoomControl);
+
+  }
+
+
+  /**
    * Render the image container.
    */
   render() {
-    return <div id="image"></div>;
+    return <div id="image" ref="image"></div>;
   }
 
 
