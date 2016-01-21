de12ea7c2ecef6839161dbb1e43da55beb27df03
David McClure <davidwilliammcclure@gmail.com>
2016-01-20 19:46:11 -0800

    Basic React/Redux rig for the graph.

---
+++ osp/www/assets/javascripts/graph/components/app.js
@@ -0,0 +1,21 @@
+
+
+import React, { Component } from 'react';
+
+
+export default class extends Component {
+
+
+  /**
+   * Render the application.
+   */
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>graph</h1>
+      </div>
+    );
+  }
+
+
+}
---
+++ osp/www/assets/javascripts/graph/reducers/index.js
@@ -0,0 +1,10 @@
+
+
+import { combineReducers } from 'redux';
+
+import image from './image';
+
+
+export default combineReducers({
+  image,
+});
