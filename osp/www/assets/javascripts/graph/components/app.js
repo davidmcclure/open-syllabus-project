1f02e1bfde03c3bce3caf7a62e2f366756d76554
David McClure <davidwilliammcclure@gmail.com>
2016-01-21 10:13:07 -0800

    Add a simple <Header /> component.

--- osp/www/assets/javascripts/graph/components/app.js
+++ osp/www/assets/javascripts/graph/components/app.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 
 import Image from './image';
+import Header from './header';
 
 
 export default class extends Component {
@@ -15,6 +16,7 @@ export default class extends Component {
     return (
       <div className="wrapper">
         <Image />
+        <Header />
       </div>
     );
   }
---
+++ osp/www/assets/javascripts/graph/components/header.js
@@ -0,0 +1,24 @@
+
+
+import React, { Component } from 'react';
+
+
+export default class extends Component {
+
+
+  /**
+   * Render the header.
+   */
+  render() {
+    return (
+      <div id="header">
+        <h1>
+          <a href="/">Open Syllabus Explorer</a>
+          <code>beta</code>
+        </h1>
+      </div>
+    );
+  }
+
+
+}
