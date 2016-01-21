b8596114d09c3b85e9b91897064c552d71e8f5fb
David McClure <davidwilliammcclure@gmail.com>
2016-01-21 08:31:11 -0800

    Render the field boxes.

--- osp/www/assets/javascripts/graph/components/fields.js
+++ osp/www/assets/javascripts/graph/components/fields.js
@@ -1,6 +1,9 @@
 
 
+import _ from 'lodash';
 import React, { Component, PropTypes } from 'react';
+
+import config from './fields.yml';
 
 
 export default class extends Component {
@@ -15,7 +18,37 @@ export default class extends Component {
    * Mount the field boxes.
    */
   componentDidMount() {
-    console.log('fields');
+
+    _.each(config, f => {
+
+      let pts = [
+        [f.tly, f.tlx],
+        [f.tly, f.brx],
+        [f.bry, f.brx],
+        [f.bry, f.tlx],
+        [f.tly, f.tlx],
+      ];
+
+      let opts = {
+        color: '#ffc600',
+        opacity: 1,
+        weight: 0.8,
+      };
+
+      let box = L.polyline(pts, opts);
+
+      let icon = L.divIcon({
+        html: f.label,
+        iconSize: null,
+      });
+
+      let marker = L.marker([f.bry, f.tlx], { icon });
+
+      this.context.map.addLayer(box);
+      this.context.map.addLayer(marker);
+
+    });
+
   }
 
 
