b4ead772f76dcdb9f4d01f177f2c14164dc4aa07
David McClure <davidwilliammcclure@gmail.com>
2016-01-22 08:38:53 -0800

    Adds a descriptive tagline to the graph.

--- osp/www/assets/javascripts/graph/components/header.js
+++ osp/www/assets/javascripts/graph/components/header.js
@@ -12,10 +12,15 @@ export default class extends Component {
   render() {
     return (
       <div id="header">
+
         <h1>
           <a href="/">Open Syllabus Explorer</a>
           <code>beta</code>
         </h1>
+
+        <p>The top ~10,000 texts. Texts assigned on the same syllabi are
+        clustered together.</p>
+
       </div>
     );
   }
