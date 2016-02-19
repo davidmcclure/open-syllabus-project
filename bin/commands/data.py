9a1d9bf42eb10e0de7b518a968ff05b408ede9d0
David McClure <davidwilliammcclure@gmail.com>
2016-02-19 10:01:51 -0800

    Write all field rankings.

--- bin/commands/data.py
+++ bin/commands/data.py
@@ -1,10 +1,13 @@
 
 
+import os
 import click
 import json
 
 from osp.www import utils
 from osp.www.hit import Hit
+
+from slugify import slugify
 
 
 @click.group()
@@ -23,6 +26,31 @@ def overall(out_file, n):
 
     ranks = utils.rank_texts.uncached(size=n)
     dump_ranks(ranks, out_file)
+
+
+@cli.command()
+@click.argument('path', type=click.Path(exists=True))
+@click.option('--n', default=10000)
+def fields(path, n):
+
+    """
+    Write overall rankings.
+    """
+
+    fields = utils.field_facets()
+
+    for f in fields:
+
+        ranks = utils.rank_texts.uncached(
+            filters=dict(field_id=f['value']),
+            size=n,
+        )
+
+        fname = '{0}.json'.format(slugify(f['label']))
+        fpath = os.path.join(path, fname)
+
+        with open(fpath, 'w') as fh:
+            dump_ranks(ranks, fh)
 
 
 def dump_ranks(ranks, fh):
