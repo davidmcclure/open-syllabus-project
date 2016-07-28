2b06234f084927d43db667528ff9206b9b08ca43
David McClure <davidwilliammcclure@gmail.com>
2016-07-28 08:25:45 -0700

    Add a seed_to_regex() helper, which generates a regex from a URL that matches 'child' URLs.

---
+++ osp/institutions/utils.py
@@ -0,0 +1,52 @@
+
+
+import tldextract
+import re
+
+from urllib.parse import urlparse
+
+
+def seed_to_regex(seed):
+
+    """
+    Given a URL, make a regex that matches child URLs.
+
+    Args:
+        seed (str)
+
+    Returns: regex
+    """
+
+    parsed = urlparse(seed)
+
+    # 1 -- http(s)://
+
+    protocol = '^https?{0}'.format(re.escape('://'))
+
+    # 2 -- If the seed has a non-www subdomain, require a matching subdomain.
+
+    subdomain = '[^\/]*'
+
+    tld = tldextract.extract(seed)
+
+    if tld.subdomain and tld.subdomain != 'www':
+        subdomain = re.escape(tld.subdomain+'.')
+
+    # 3 -- yale.edu
+
+    netloc = re.escape('.'.join([tld.domain, tld.suffix]))
+
+    # 4 -- If a path is present, require a sub-path.
+
+    path = '.*'
+
+    clean_path = parsed.path.rstrip('/')
+
+    if clean_path:
+        path = re.escape(clean_path+'/')
+
+    # Join the parts.
+
+    pattern = ''.join([protocol, subdomain, netloc, path])
+
+    return re.compile(pattern, re.I)
---
+++ osp/test/institutions/utils/test_seed_to_regex.py
@@ -0,0 +1,130 @@
+
+
+import pytest
+import re
+
+from osp.institutions.utils import seed_to_regex
+
+
+@pytest.mark.parametrize('seed,url,result', [
+
+    # Allow URLs that are "below" the seed path.
+    (
+        'http://yale.edu',
+        'http://yale.edu/page',
+        True,
+    ),
+    (
+        'http://yale.edu/root',
+        'http://yale.edu/root/page',
+        True,
+    ),
+    (
+        'http://yale.edu/root',
+        'http://yale.edu/root/page.html',
+        True,
+    ),
+
+    # Block URLs that are adjacent to or above the seed path.
+    (
+        'http://yale.edu/root1',
+        'http://yale.edu/root2',
+        False,
+    ),
+    (
+        'http://yale.edu/root1/root2',
+        'http://yale.edu/root1',
+        False,
+    ),
+    (
+        'http://yale.edu/root',
+        'http://yale.edu/root2/page',
+        False,
+    ),
+
+    # When the seed has no subdomain (or just www), allow any subdomain.
+    (
+        'http://yale.edu',
+        'http://sub.yale.edu',
+        True,
+    ),
+    (
+        'http://www.yale.edu',
+        'http://sub.yale.edu',
+        True,
+    ),
+
+    # When the seed has a non-www subdomain, the URL must have the exact same
+    # subdomain as the seed.
+    (
+        'http://sub.yale.edu',
+        'http://sub.yale.edu',
+        True,
+    ),
+    (
+        'http://sub1.yale.edu',
+        'http://sub2.yale.edu',
+        False,
+    ),
+    (
+        'http://sub1.yale.edu',
+        'http://sub2.sub1.yale.edu',
+        False,
+    ),
+
+    # Ignore casing.
+    (
+        'http://yale.edu',
+        'http://YALE.EDU',
+        True,
+    ),
+
+    # Block URLs with different hosts.
+    (
+        'http://yale.edu',
+        'http://harvard.edu',
+        False,
+    ),
+
+    # Allow different protocols.
+    (
+        'http://yale.edu',
+        'https://yale.edu',
+        True,
+    ),
+    (
+        'https://yale.edu',
+        'http://yale.edu',
+        True,
+    ),
+
+    # Ignore trailing slash on path.
+    (
+        'http://yale.edu/',
+        'http://yale.edu/page',
+        True,
+    ),
+    (
+        'http://yale.edu/root/',
+        'http://yale.edu/root/page',
+        True,
+    ),
+
+    # Block "embedded" URLs.
+    (
+        'http://yale.edu',
+        'http://facebook.com/share/http://yale.edu/page',
+        False,
+    ),
+    (
+        'http://yale.edu',
+        'http://facebook.com/share/yale.edu/page',
+        False,
+    ),
+
+])
+def test_seed_to_regex(seed, url, result):
+
+    r = seed_to_regex(seed)
+
+    assert bool(r.search(url)) == result
