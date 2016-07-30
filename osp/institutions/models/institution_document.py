6fd24b00c288c793b3758a0d5be8cc84f6dbe1a6
David McClure <davidwilliammcclure@gmail.com>
2016-07-30 10:23:47 -0700

    Stub in a DocInstPool class.

--- osp/institutions/models/institution_document.py
+++ osp/institutions/models/institution_document.py
@@ -1,13 +1,10 @@
 
 
 from peewee import ForeignKeyField
-from playhouse.postgres_ext import ServerSide
 
 from osp.common import config
 from osp.common.models.base import BaseModel
-from osp.common.utils import query_bar
 from osp.institutions.models import Institution
-from osp.institutions.utils import seed_to_regex
 from osp.corpus.models import Document
 
 
@@ -20,39 +17,3 @@ class Institution_Document(BaseModel):
 
     class Meta:
         database = config.get_table_db('institution_document')
-
-
-    @classmethod
-    def link(cls):
-
-        """
-        Link each document with an institution.
-        """
-
-        # TODO: multiprocessing?
-
-        # Map URL regex -> institution.
-        regex_to_inst = [
-            (seed_to_regex(inst.url), inst)
-            for inst in ServerSide(Institution.select())
-            if inst.url
-        ]
-
-        docs = query_bar(Document.select())
-
-        # Walk documents.
-        for i, doc in enumerate(docs):
-
-            try:
-
-                url = doc.syllabus.url
-
-                # Probe for a matching institution.
-                for regex, inst in regex_to_inst:
-                    if regex.search(url):
-
-                        # Write the link row.
-                        cls.create(institution=inst, document=doc)
-
-            except Exception as e:
-                print(e)
