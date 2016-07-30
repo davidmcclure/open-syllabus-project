b2ad510245a47b722f3c86ca9a5ac095808f599a
David McClure <davidwilliammcclure@gmail.com>
2016-07-30 11:06:57 -0700

    Add a doc_to_inst CSV writer, to check the results.

---
+++ bin/commands/inst_csv.py
@@ -0,0 +1,36 @@
+
+
+import click
+import csv
+
+from osp.institutions.models import Institution_Document
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('out_file', type=click.File('w'))
+@click.option('--n', default=None)
+def doc_to_inst(out_file, n):
+
+    """
+    Dump N institution -> document matches.
+    """
+
+    # CSV writer.
+    cols = ['inst_url', 'doc_url']
+    writer = csv.DictWriter(out_file, cols)
+    writer.writeheader()
+
+    # Pull matches.
+    matches = Institution_Document.select().limit(n)
+
+    for row in matches:
+
+        writer.writerow(dict(
+            inst_url=row.institution.url,
+            doc_url=row.document.syllabus.url,
+        ))
