d945dd27a00afdd529f33a730ceaf9245650f1f6
David McClure <davidwilliammcclure@gmail.com>
2016-07-30 18:13:18 -0700

    Get rid of the doc_to_inst CLI route.

--- bin/commands/inst.py
+++ bin/commands/inst.py
@@ -2,11 +2,9 @@
 
 import click
 
-from osp.corpus.models import Document
 from osp.common.utils import query_bar
-from osp.institutions.models import Institution
-from osp.institutions.models import Institution_Document
-from osp.institutions.jobs import doc_to_inst
+from osp.institutions.models import Institution, Institution_Document
+from osp.corpus.models import Document
 
 from peewee import create_model_tables
 
@@ -38,15 +36,3 @@ def ingest():
 
     Institution.ingest_usa()
     Institution.ingest_world()
-
-
-@cli.command()
-def run_doc_to_inst():
-
-    """
-    Match documents -> institutions.
-    """
-
-    for doc in query_bar(Document.select()):
-        try: doc_to_inst(doc.id)
-        except: pass
