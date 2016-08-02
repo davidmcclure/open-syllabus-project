332028938df33376a08ab2d0e81401cbd322a748
David McClure <davidwilliammcclure@gmail.com>
2016-08-02 00:57:19 -0700

    Re-add the usa_doc_counts CLI command.

--- bin/commands/inst_csv.py
+++ bin/commands/inst_csv.py
@@ -3,7 +3,10 @@
 import click
 import csv
 
-from osp.institutions.models import Institution_Document
+from peewee import fn
+
+from osp.common.utils import read_csv
+from osp.institutions.models import Institution, Institution_Document
 
 
 @click.group()
@@ -34,3 +37,34 @@ def doc_to_inst(out_file, n):
             inst_url=row.institution.url,
             doc_url=row.document.syllabus.url,
         ))
+
+
+@cli.command()
+@click.argument('out_file', type=click.File('w'))
+def usa_doc_counts(out_file):
+
+    """
+    Update the institutions CSV with the number of docs in the OSP corpus.
+    """
+
+    reader = read_csv('osp.institutions', 'data/usa.csv')
+
+    cols = reader.fieldnames + ['osp_doc_count']
+
+    writer = csv.DictWriter(out_file, cols)
+    writer.writeheader()
+
+    for row in reader:
+
+        count = fn.count(Institution_Document.id)
+
+        query = (
+            Institution_Document
+            .select(count)
+            .join(Institution)
+            .where(Institution.url == row['web_url'])
+        )
+
+        row['osp_doc_count'] = query.scalar()
+
+        writer.writerow(row)
