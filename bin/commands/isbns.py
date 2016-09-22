d953dcd581b39051e85849e49be93cb1bc0d1725
David McClure <davidwilliammcclure@gmail.com>
2016-09-21 21:39:29 -0700

    Map control number -> ISBN.

--- bin/commands/isbns.py
+++ bin/commands/isbns.py
@@ -1,6 +1,11 @@
 
 
 import click
+import pickle
+
+from osp.citations.hlom_corpus import HLOM_Corpus
+from osp.citations.hlom_record import HLOM_Record
+
 
 @click.group()
 def cli():
@@ -8,10 +13,26 @@ def cli():
 
 
 @cli.command()
-def control_to_isbn():
+@click.argument('out_file', type=click.File('wb'))
+def control_to_isbn(out_file):
 
     """
     Map control numbers -> ISBNs.
     """
 
-    print('test')
+    corpus = HLOM_Corpus.from_env()
+
+    isbns = {}
+    for i, marc in enumerate(corpus.records()):
+
+        record = HLOM_Record(marc)
+
+        isbn = marc.isbn()
+
+        if isbn:
+            isbns[record.control_number] = isbn
+
+        if i%1000 == 0:
+            print(i)
+
+    pickle.dump(isbns, out_file)
