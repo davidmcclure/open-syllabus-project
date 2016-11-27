5ca849a4e06f086cc12cc4d211f59c6cee39161c
David McClure <davidwilliammcclure@gmail.com>
2016-11-26 19:31:50 -0800

    In query_urls, query ES, lookup URL, write CSV.

--- bin/commands/ecology.py
+++ bin/commands/ecology.py
@@ -2,6 +2,7 @@
 
 import click
 import pickle
+import csv
 
 from osp.corpus.models import Document
 from osp.common import config
@@ -29,11 +30,49 @@ def doc_to_url(out_file):
 
 @cli.command()
 @click.argument('query', type=str)
-@click.argument('in_file', type=click.File('rb'))
-def query_urls(query, in_file):
+@click.argument('urls_file', type=click.File('rb'))
+@click.argument('csv_file', type=click.File('w'))
+def query_urls(query, urls_file, csv_file):
 
     """
     Query docs for a keyword, join URLs, write to CSV.
     """
 
-    print(query)
+    urls = pickle.load(urls_file)
+
+    # Query for docs.
+    results = config.es.search(
+
+        index='document',
+        request_timeout=90,
+
+        body={
+            'fields': [],
+            'size': 10,
+            'query': {
+                'match': {
+                    'body': query
+                }
+            },
+        },
+
+    )
+
+    writer = csv.DictWriter(csv_file, [
+        'id',
+        'score',
+        'url',
+    ])
+
+    writer.writeheader()
+
+    if results['hits']['total'] > 0:
+        for hit in results['hits']['hits']:
+
+            url = urls.get(int(hit['_id']))
+
+            writer.writerow(dict(
+                id=hit['_id'],
+                score=hit['_score'],
+                url=url,
+            ))
